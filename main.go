abf07b12f4080135dd47526851c6da4a770dd51b
Alex Komoroske <jkomoros@gmail.com>
2017-01-28 16:05:36 -0800

    Introduce a notion of JSONObject, and introduce (and test) a JSON() method on State.

--- main.go
+++ main.go
@@ -1,5 +1,9 @@
 package boardgame
 
+import (
+	"encoding/json"
+)
+
 type State struct {
 	//The version number of the state. Increments by one each time a Move is
 	//applied.
@@ -14,10 +18,13 @@ type State struct {
 	Users []UserState
 }
 
+//A JSONObject is an object that is ready to be serialized to JSON.
+type JSONObject map[string]interface{}
+
 type JSONer interface {
 	//Returns the canonical JSON representation of this object, suitable to
 	//being communicated across the wire or saved in a DB.
-	JSON() []byte
+	JSON() JSONObject
 }
 
 //Property reader is a way to read out properties on an object with unknown
@@ -45,4 +52,34 @@ type UserState interface {
 type GameState interface {
 	JSONer
 	PropertyReader
+}
+
+//Serialize converts the JSONObject into bytes, suitable for being transferred
+//across the wire or written to disk.
+func (j *JSONObject) Serialize() []byte {
+	result, err := json.Marshal(j)
+	if err != nil {
+		return []byte("")
+	}
+	return result
+}
+
+//JSON returns the JSONObject representing the State's full state.
+func (s *State) JSON() JSONObject {
+	result := make(JSONObject)
+	result["Version"] = s.Version
+	result["Schema"] = s.Schema
+	if s.Game != nil {
+		result["Game"] = s.Game.JSON()
+	}
+	if s.Users == nil {
+		return result
+	}
+	array := make([]interface{}, len(s.Users))
+	for i, user := range s.Users {
+		array[i] = user.JSON()
+	}
+	result["Users"] = array
+
+	return result
 }
--- main_test.go
+++ main_test.go
@@ -1,6 +1,8 @@
 package boardgame
 
 import (
+	"encoding/json"
+	"io/ioutil"
 	"testing"
 )
 
@@ -15,4 +17,31 @@ func TestState(t *testing.T) {
 	if state == nil {
 		t.Error("State could not be created")
 	}
+
+	json := state.JSON()
+	golden := goldenJSON("empty_state.json", t)
+
+	compareJSONObjects(json, golden, "Empty state", t)
+}
+
+func compareJSONObjects(in JSONObject, golden JSONObject, message string, t *testing.T) {
+	if string(in.Serialize()) != string(golden.Serialize()) {
+		t.Error("Got wrong json.", message, "Got", in, "wanted", golden)
+	}
+}
+
+func goldenJSON(fileName string, t *testing.T) JSONObject {
+	contents, err := ioutil.ReadFile("./test/" + fileName)
+	if err != nil {
+		t.Fatal("Couldn't load golden JSON at " + fileName)
+	}
+
+	result := make(JSONObject)
+
+	if err := json.Unmarshal(contents, &result); err != nil {
+		t.Fatal("Couldn't parse golden json at " + fileName + err.Error())
+	}
+
+	return result
+
 }
