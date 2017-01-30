b3a3469dd3579387f3c71c507bbab2d8c258216a
Alex Komoroske <jkomoros@gmail.com>
2017-01-29 16:16:25 -0800

    Started tictactoe example package (curently just a package comment)

---
+++ examples/tictactoe/main.go
@@ -0,0 +1,8 @@
+/*
+
+Package tictactoe is an exceedingly simple game based on boardgame. It serves
+as an example, and also helps verify that the design and implementation of
+boardgame are useful for real games.
+
+*/
+package tictactoe
