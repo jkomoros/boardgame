03c1f542508a5ec1edc6bb75275eeb52b85f4c6a
Alex Komoroske <jkomoros@gmail.com>
2017-01-30 06:16:16 -0800

    Add a userState that holds unused tokens.

--- examples/tictactoe/state.go
+++ examples/tictactoe/state.go
@@ -26,9 +26,36 @@ func (g *gameState) JSON() boardgame.JSONObject {
 	return g
 }
 
+type userState struct {
+	playerIndex  int
+	UnusedTokens *boardgame.GrowableStack
+}
+
+func (u *userState) Props() []string {
+	return boardgame.PropertyReaderPropsImpl(u)
+}
+
+func (u *userState) Prop(name string) interface{} {
+	return boardgame.PropertyReaderPropImpl(u, name)
+}
+
+func (u *userState) Copy() boardgame.UserState {
+	var result userState
+	result = *u
+	return &result
+}
+
+func (u *userState) JSON() boardgame.JSONObject {
+	return u
+}
+
+func (u *userState) PlayerIndex() int {
+	return u.playerIndex
+}
+
 type statePayload struct {
-	game *gameState
-	//we have no user state because all state is public.
+	game  *gameState
+	users []*userState
 }
 
 func (s *statePayload) Game() boardgame.GameState {
@@ -36,17 +63,38 @@ func (s *statePayload) Game() boardgame.GameState {
 }
 
 func (s *statePayload) Users() []boardgame.UserState {
-	return nil
+	array := make([]boardgame.UserState, len(s.users))
+
+	for i := 0; i < len(s.users); i++ {
+		array[i] = s.users[i]
+	}
+
+	return array
 }
 
 func (s *statePayload) JSON() boardgame.JSONObject {
+
+	array := make([]boardgame.JSONObject, len(s.users))
+
+	for i, user := range s.users {
+		array[i] = user.JSON()
+	}
+
 	return boardgame.JSONMap{
-		"Game": s.game.JSON(),
+		"Game":  s.game.JSON(),
+		"Users": array,
 	}
 }
 
 func (s *statePayload) Copy() boardgame.StatePayload {
+	array := make([]*userState, len(s.users))
+
+	for i := 0; i < len(s.users); i++ {
+		array[i] = s.users[i].Copy().(*userState)
+	}
+
 	return &statePayload{
-		game: s.game.Copy().(*gameState),
+		game:  s.game.Copy().(*gameState),
+		users: array,
 	}
 }
