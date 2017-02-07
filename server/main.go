e43e4858763da2408f4de4f82dd68cfe60f96bdb
Alex Komoroske <jkomoros@gmail.com>
2017-02-07 06:08:56 -0800

    Rendering is done via a template. Use a hacky way to reference it. Part of #52.

--- server/main.go
+++ server/main.go
@@ -1,11 +1,14 @@
 package server
 
 import (
-	"fmt"
 	"github.com/jkomoros/boardgame"
+	"html/template"
 	"log"
 	"net/http"
+	"os"
 )
+
+type templateArgs map[string]interface{}
 
 type Server struct {
 	game *boardgame.Game
@@ -17,8 +20,35 @@ func NewServer(game *boardgame.Game) *Server {
 	}
 }
 
+const (
+	pathToLib = "$GOPATH/src/github.com/jkomoros/boardgame/server/"
+)
+
 func (s *Server) viewHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, string(boardgame.Serialize(s.game.State.JSON())))
+
+	args := make(templateArgs)
+
+	args["State"] = string(boardgame.Serialize(s.game.State.JSON()))
+
+	s.renderTemplate(w, "main", args)
+
+}
+
+func (s *Server) renderTemplate(w http.ResponseWriter, tmpl string, args templateArgs) {
+	//TODO: this seems brittle!
+	t, err := template.ParseFiles(os.ExpandEnv(pathToLib) + "templates/" + tmpl + ".tmpl")
+
+	if err != nil {
+		panic("Couldn't find template " + tmpl + " " + err.Error())
+	}
+
+	if args == nil {
+		args = make(templateArgs)
+	}
+
+	args["Game"] = s.game
+
+	t.Execute(w, args)
 }
 
 //Start is where you start the server, and it never returns until it's time to shut down.
