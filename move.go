0ec24ed202c86f1292ef61bbe565360afd3fae46
Alex Komoroske <jkomoros@gmail.com>
2017-02-10 17:04:34 -0800

    Remove a completed TODO

--- move.go
+++ move.go
@@ -7,7 +7,6 @@ type Move interface {
 	//Legal returns nil if this proposed move is legal, or an error if the
 	//move is not legal
 	Legal(state StatePayload) error
-	//TODO: figure out how to get a string describing why it's not legal out
 
 	//Apply applies the move to the state and returns a new state object. It
 	//should not be called directly; use Game.ApplyMove.
