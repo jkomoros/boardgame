e4b45ef76675bfc9ad13e6d8b9b987543883b4dc
Alex Komoroske <jkomoros@gmail.com>
2017-02-11 13:01:41 -0800

    Component.Values is not just a generic PropertyReader, but a ComponentValues interface.

--- component.go
+++ component.go
@@ -4,9 +4,13 @@ package boardgame
 //resource tokens, etc are all components. Values is a struct that stores the
 //specific values for the component.
 type Component struct {
-	Values PropertyReader
+	Values ComponentValues
 	//The deck we're a part of.
 	Deck *Deck
 	//The index we are in the deck we're in.
 	DeckIndex int
+}
+
+type ComponentValues interface {
+	PropertyReader
 }
