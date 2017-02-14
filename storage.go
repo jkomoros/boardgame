c5b621a53a8dba5902cc2aed4dbd996f2b9d314c
Alex Komoroske <jkomoros@gmail.com>
2017-02-13 17:25:54 -0800

    Implementation for inMemoryStorageManager.State(). Part of #81.

--- storage.go
+++ storage.go
@@ -1,5 +1,9 @@
 package boardgame
 
+import (
+	"errors"
+)
+
 //StorageManager is an interface that anything can implement to handle the
 //persistence of Games and States.
 type StorageManager interface {
@@ -9,4 +13,61 @@ type StorageManager interface {
 	//SaveState puts the given stateWrapper (at the specified version and
 	//schema) into storage.
 	SaveState(game *Game, state *StateWrapper) error
+}
+
+type memoryStateRecord struct {
+	Schema          int
+	Version         int
+	SerializedState []byte
+}
+
+type inMemoryStorageManager struct {
+	states map[string]map[int]*memoryStateRecord
+}
+
+func NewInMemoryStorageManager() StorageManager {
+	//InMemoryStorageManager is an extremely simple StorageManager that just keeps
+	//track of the objects in memory.
+	return &inMemoryStorageManager{
+		states: make(map[string]map[int]*memoryStateRecord),
+	}
+}
+
+func (i *inMemoryStorageManager) State(game *Game, version int) *StateWrapper {
+	if game == nil {
+		return nil
+	}
+
+	if version < 0 || version > game.StateWrapper.Version {
+		return nil
+	}
+
+	versionMap, ok := i.states[game.Id()]
+
+	if !ok {
+		return nil
+	}
+
+	record, ok := versionMap[version]
+
+	if !ok {
+		return nil
+	}
+
+	state, err := game.Delegate.StateFromBlob(record.SerializedState, record.Schema)
+
+	if err != nil {
+		return nil
+	}
+
+	return &StateWrapper{
+		Version: record.Version,
+		Schema:  record.Schema,
+		State:   state,
+	}
+}
+
+func (i *inMemoryStorageManager) SaveState(game *Game, state *StateWrapper) error {
+	//TODO: do something
+	return errors.New("That method is not yet implemented")
 }
