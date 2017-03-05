269387c1ca2697f3d3f6bf46b072dfdd18a0e893
Alex Komoroske <jkomoros@gmail.com>
2017-03-04 21:09:38 -0800

    Test more of the machinery of State.Computed(). Part of #146.

--- computed_test.go
+++ computed_test.go
@@ -4,6 +4,15 @@ import (
 	"testing"
 )
 
+type stateComputeDelegate struct {
+	testGameDelegate
+	config *ComputedPropertiesConfig
+}
+
+func (s *stateComputeDelegate) ComputedPropertiesConfig() *ComputedPropertiesConfig {
+	return s.config
+}
+
 func TestComputedPropertyDefinitionCompute(t *testing.T) {
 
 	game := testGame()
@@ -61,6 +70,90 @@ func TestComputedPropertyDefinitionCompute(t *testing.T) {
 		} else if val != playerState.Score {
 			t.Error("Unexpected score was not real value. Got", val, "wanted", playerState.Score)
 		}
+	}
+
+}
+
+func TestStateComputed(t *testing.T) {
+
+	delegate := &stateComputeDelegate{}
+
+	manager := NewGameManager(delegate, newTestGameChest(), newTestStorageManager())
+
+	manager.SetUp()
+
+	game := NewGame(manager)
+
+	game.SetUp(0)
+
+	state := game.CurrentState()
+
+	gameState, playerStates := concreteStates(state)
+
+	gameState.CurrentPlayer = 4
+
+	playerStates[0].Score = 10
+	playerStates[1].Score = 5
+
+	config := &ComputedPropertiesConfig{
+		Properties: map[string]ComputedPropertyDefinition{
+			"CurrentPlayerPlusFive": ComputedPropertyDefinition{
+				Dependencies: []StatePropertyRef{
+					{
+						Group:    StateGroupGame,
+						PropName: "CurrentPlayer",
+					},
+				},
+				PropType: TypeInt,
+				Compute: func(shadow *ShadowState) (interface{}, error) {
+					val, err := shadow.Game.IntProp("CurrentPlayer")
+					if err != nil {
+						return nil, err
+					}
+					return val + 5, nil
+				},
+			},
+			"SumAllScores": ComputedPropertyDefinition{
+				Dependencies: []StatePropertyRef{
+					{
+						Group:    StateGroupPlayer,
+						PropName: "Score",
+					},
+				},
+				PropType: TypeInt,
+				Compute: func(shadow *ShadowState) (interface{}, error) {
+					result := 0
+					for _, player := range shadow.Players {
+						val, err := player.IntProp("Score")
+
+						if err != nil {
+							return nil, err
+						}
+
+						result += val
+					}
+					return result, nil
+				},
+			},
+		},
+	}
+
+	delegate.config = config
+
+	computed := state.Computed()
+
+	if val, err := computed.IntProp("CurrentPlayerPlusFive"); err != nil {
+		t.Error("Unexpected error retrieving CurrentPlayerPlusFive", err)
+	} else {
+		if val != 4+5 {
+			t.Error("CurrentPlayerPlusFive was unexpected value. Wanted", 4+5, "got", val)
+		}
+	}
+
+	if val, err := computed.IntProp("SumAllScores"); err != nil {
+		t.Error("Unexpected error retrieving SumAllScores", err)
+	} else if val != 15 {
+		t.Error("Unexpected result for SumAllScores. Got", val, "wanted", 15)
 	}
 
 }
