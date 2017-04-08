2f2c710f844e4a54885a56917d5506f464aa06a5
Alex Komoroske <jkomoros@gmail.com>
2017-04-07 20:57:47 -0700

    Timers grow an ID field. Part of #178.

--- timer.go
+++ timer.go
@@ -1,7 +1,10 @@
 package boardgame
 
 type Timer struct {
-	//TODO: actually store fields
+	//ID will be an opaque identifier that is used to keep track of the
+	//corresponding underlying Timer object in the game engine. It is not
+	//meaningful to inspect yourself and should not be modified.
+	ID int
 }
 
 func NewTimer() *Timer {
@@ -12,4 +15,9 @@ func (t *Timer) Copy() *Timer {
 	var result Timer
 	result = *t
 	return &result
+}
+
+//Active returns true if the timer is active and counting down.
+func (t *Timer) Active() bool {
+	return t.ID == 0
 }
