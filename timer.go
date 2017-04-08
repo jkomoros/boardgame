7043c3ceae1a2967cc631efa613c1a3c6d206b57
Alex Komoroske <jkomoros@gmail.com>
2017-04-08 09:32:14 -0700

    Implemented gameManager stubs for registerTimer, cancelTimer, getTimer. Timer's Start, Cancel, TimeLeft methods hook into them. Part of #178.

--- timer.go
+++ timer.go
@@ -37,14 +37,17 @@ func (t *Timer) TimeLeft() int {
 	if !t.Active() {
 		return 0
 	}
-	//TODO: when this is actually hooked up, return a real value.
-	return 0
+
+	return t.statePtr.game.manager.getTimer(t.Id)
 }
 
 //Start starts the timer. After nanoseconds nanoseconds have passed, the Move
 //will be proposed via proposeMove.
 func (t *Timer) Start(nanoseconds int, move Move) {
-	//TODO: actually do something here
+	game := t.statePtr.game
+	manager := game.manager
+
+	t.Id = manager.registerTimer(nanoseconds, game, move)
 }
 
 //Cancel cancels an active timer. If the timer is not active, it has no
@@ -54,6 +57,39 @@ func (t *Timer) Cancel() bool {
 	if !t.Active() {
 		return false
 	}
-	//TODO: tell the manager to cancel here.
+	manager := t.statePtr.game.manager
+
+	manager.cancelTimer(t.Id)
+
+	t.Id = 0
+
 	return true
+}
+
+/**************************************
+ *
+ * GameManager timer-related methods
+ *
+ **************************************/
+
+//registerTimer registers an implementation level-timer, so that after
+//nanoseconds have passed the given move will be proposed to the given game.
+//Returns an Id that can be used ot cancel or fetch information on the timer
+//in the future. Only designed to be called from Timer.Start()
+func (g *GameManager) registerTimer(nanoseconds int, game *Game, move Move) int {
+	//TOOD: actually implement this
+	return 0
+}
+
+//cancelTimer cancels the timer with the given Id. IF there there is no such
+//timer, or that timer is already over, it is a no-op.
+func (g *GameManager) cancelTimer(id int) {
+	//TODO: actually implement this.
+}
+
+//getTimer returns the number of nanoseconds left for the timer with the given
+//id.
+func (g *GameManager) getTimer(id int) int {
+	//TODO: actually implement this.
+	return 0
 }
