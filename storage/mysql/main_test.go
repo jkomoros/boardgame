c44b216b04d5f92d6d230ff366aca480ff9a804c
Alex Komoroske <jkomoros@gmail.com>
2017-04-18 06:40:46 -0700

    Implemented local GameStorageRecord and the conversion passes for them. Part of #255.

--- storage/mysql/main_test.go
+++ storage/mysql/main_test.go
@@ -1,7 +1,9 @@
 package mysql
 
 import (
+	"github.com/jkomoros/boardgame"
 	"github.com/jkomoros/boardgame/storage/test"
+	"github.com/workfit/tester/assert"
 	"testing"
 )
 
@@ -11,4 +13,50 @@ func TestStorageManager(t *testing.T) {
 		return NewStorageManager(true)
 	}, "mysql", "root:root@tcp(localhost:3306)/boardgame_test", t)
 
+}
+
+func TestWinnersConversion(t *testing.T) {
+	tests := []struct {
+		input       string
+		result      []boardgame.PlayerIndex
+		expectError bool
+	}{
+		{
+			"",
+			nil,
+			false,
+		},
+		{
+			"1,2",
+			[]boardgame.PlayerIndex{1, 2},
+			false,
+		},
+		{
+			"-1",
+			[]boardgame.PlayerIndex{-1},
+			false,
+		},
+		{
+			"1,2,",
+			nil,
+			true,
+		},
+	}
+
+	for i, test := range tests {
+		winners, err := stringToWinners(test.input)
+
+		if test.expectError {
+			assert.For(t, i).ThatActual(err).IsNotNil()
+			continue
+		} else {
+			assert.For(t, i).ThatActual(err).IsNil()
+		}
+
+		assert.For(t, i).ThatActual(winners).Equals(test.result).ThenDiffOnFail()
+
+		reInput := winnersToString(test.result)
+
+		assert.For(t, i).ThatActual(reInput).Equals(test.input)
+	}
 }
--- storage/mysql/structs.go
+++ storage/mysql/structs.go
@@ -1,7 +1,11 @@
 package mysql
 
 import (
+	"errors"
+	"github.com/jkomoros/boardgame"
 	"github.com/jkomoros/boardgame/server/api/users"
+	"strconv"
+	"strings"
 )
 
 //We define our own records, primarily to decorate with tags for Gorp, but
@@ -10,6 +14,86 @@ import (
 
 type UserStorageRecord struct {
 	Id string `db:",size:16"`
+}
+
+type GameStorageRecord struct {
+	Name     string `db:",size:64"`
+	Id       string `db:",size:16"`
+	Version  int64
+	Winners  string `db:",size:128"`
+	Finished bool
+	//NumPlayers is the reported number of players when it was created.
+	//Primarily for convenience to storage layer so they know how many players
+	//are in the game.
+	NumPlayers int64
+}
+
+func winnersToString(winners []boardgame.PlayerIndex) string {
+	if winners == nil {
+		return ""
+	}
+	strs := make([]string, len(winners))
+	for i, player := range winners {
+		strs[i] = player.String()
+	}
+	return strings.Join(strs, ",")
+}
+
+func stringToWinners(winners string) ([]boardgame.PlayerIndex, error) {
+
+	if winners == "" {
+		return nil, nil
+	}
+
+	strs := strings.Split(winners, ",")
+
+	result := make([]boardgame.PlayerIndex, len(strs))
+
+	for i, str := range strs {
+		intIndex, err := strconv.Atoi(str)
+		if err != nil {
+			return nil, errors.New("couldn't decode " + strconv.Itoa(i) + " player index: " + err.Error())
+		}
+		result[i] = boardgame.PlayerIndex(intIndex)
+	}
+
+	return result, nil
+
+}
+
+func (g *GameStorageRecord) ToStorageRecord() *boardgame.GameStorageRecord {
+
+	if g == nil {
+		return nil
+	}
+
+	winners, err := stringToWinners(g.Winners)
+
+	if err != nil {
+		return nil
+	}
+
+	return &boardgame.GameStorageRecord{
+		Name:       g.Name,
+		Id:         g.Id,
+		Version:    int(g.Version),
+		Winners:    winners,
+		NumPlayers: int(g.NumPlayers),
+	}
+}
+
+func NewGameStorageRecord(game *boardgame.GameStorageRecord) *GameStorageRecord {
+	if game == nil {
+		return nil
+	}
+
+	return &GameStorageRecord{
+		Name:       game.Name,
+		Id:         game.Id,
+		Version:    int64(game.Version),
+		Winners:    winnersToString(game.Winners),
+		NumPlayers: int64(game.NumPlayers),
+	}
 }
 
 func (s *UserStorageRecord) ToStorageRecord() *users.StorageRecord {
