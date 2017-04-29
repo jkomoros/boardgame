37ec414352a56738a1aec3c6e1dcfec3d84f99dc
Alex Komoroske <jkomoros@gmail.com>
2017-04-29 08:12:33 -0700

    Initial stubbiest of stubs of autoreader. Part of #301.

---
+++ autoreader/main.go
@@ -0,0 +1,16 @@
+/*
+
+	Autoreader is a simple program, designed to be run from go:generate, that
+	helps generate the annoying boilerplate to implement
+	boardgame.PropertyReader and boardgame.PropertyReadSetter.
+
+*/
+package main
+
+import (
+	"log"
+)
+
+func main() {
+	log.Println("Autoreader.")
+}
