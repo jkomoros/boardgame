64d91950618a873a5151fcc5065763fe4e790f6d
Alex Komoroske <jkomoros@gmail.com>
2017-04-29 08:39:13 -0700

    Define readerForStruct. Part of #301.

--- autoreader/main.go
+++ autoreader/main.go
@@ -8,9 +8,45 @@
 package main
 
 import (
+	"bytes"
+	"fmt"
 	"log"
+	"text/template"
 )
 
-func main() {
-	log.Println("Autoreader.")
+var readerTemplate *template.Template
+
+type templateConfig struct {
+	FirstLetter string
+	StructName  string
 }
+
+func init() {
+	readerTemplate = template.Must(template.New("reader").Parse(readerTemplateText))
+}
+
+func main() {
+	fmt.Println(readerForStruct("myStruct"))
+}
+
+func readerForStruct(structName string) string {
+	buf := new(bytes.Buffer)
+
+	err := readerTemplate.Execute(buf, templateConfig{
+		FirstLetter: structName[:1],
+		StructName:  structName,
+	})
+
+	if err != nil {
+		log.Println(err)
+	}
+
+	return buf.String()
+}
+
+const readerTemplateText = `
+func ({{.FirstLetter}} *{{.StructName}}) Reader() boardgame.PropertyReader {
+	return boardgame.DefaultReader({{.FirstLetter}})
+}
+
+`
