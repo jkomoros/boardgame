e8724eca6f143713a65c33fd99e60b6c7a5aedb8
Alex Komoroske <jkomoros@gmail.com>
2017-04-29 09:44:37 -0700

    Actually output to the specified file. Part of #301.

--- cmd/autoreader/main.go
+++ cmd/autoreader/main.go
@@ -14,6 +14,7 @@ import (
 	"fmt"
 	"github.com/MarcGrol/golangAnnotations/parser"
 	"io"
+	"io/ioutil"
 	"log"
 	"os"
 	"strings"
@@ -77,7 +78,7 @@ func process(options *appOptions, errOut io.ReadWriter) {
 		return
 	}
 
-	fmt.Fprintln(errOut, output)
+	ioutil.WriteFile(options.OutputFile, []byte(output), 0644)
 }
 
 func processPackage(location string) (output string, err error) {
