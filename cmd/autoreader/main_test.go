ffc4a7900a405fea556a1eca008dbe3689cae505
Alex Komoroske <jkomoros@gmail.com>
2017-04-29 09:53:41 -0700

    Add testing for autoreader. Part of #301.

---
+++ cmd/autoreader/main_test.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"bytes"
+	"github.com/workfit/tester/assert"
+	"io/ioutil"
+	"testing"
+)
+
+func TestOutput(t *testing.T) {
+	options := &appOptions{
+		PrintToConsole:   true,
+		PackageDirectory: "examplepkg/",
+	}
+
+	out := &bytes.Buffer{}
+	errOut := &bytes.Buffer{}
+
+	process(options, out, errOut)
+
+	expectedBytes, err := ioutil.ReadFile("test/output.txt")
+
+	assert.For(t).ThatActual(err).IsNil()
+
+	assert.For(t).ThatActual(out.String()).Equals(string(expectedBytes)).ThenDiffOnFail()
+
+}
