d4ef1e1b6c49090260861c2e29dcf845bb769660
Alex Komoroske <jkomoros@gmail.com>
2017-05-13 19:50:19 -0700

    Start scratching out agentState with things like CardSeen. Part of #289.

--- examples/memory/agent.go
+++ examples/memory/agent.go
@@ -6,6 +6,16 @@ import (
 
 type Agent struct{}
 
+type agentCardInfo struct {
+	Value string
+	Index int
+}
+
+type agentState struct {
+	LastCards    []agentCardInfo
+	MemoryLength int
+}
+
 func (a *Agent) Name() string {
 	return "ai"
 }
@@ -22,4 +32,33 @@ func (a *Agent) SetUpForGame(game *boardgame.Game, player boardgame.PlayerIndex)
 func (a *Agent) ProposeMove(game *boardgame.Game, player boardgame.PlayerIndex, agentState []byte) (move boardgame.Move, newState []byte) {
 	//TODO: do something
 	return nil, nil
+}
+
+//CardSeen is called when a card is visible. If will return true if that was
+//new information, or false if not.
+func (a *agentState) CardSeen(value string, index int) bool {
+
+	//Is this card currently in the known set of cards?
+	for _, card := range a.LastCards {
+		if card.Value == value && card.Index == index {
+			return false
+		}
+	}
+
+	//Add it to the list.
+
+	info := agentCardInfo{
+		Value: value,
+		Index: index,
+	}
+
+	a.LastCards = append([]agentCardInfo{info}, a.LastCards...)
+
+	if len(a.LastCards) > a.MemoryLength {
+		//Trim it if there are more cards than we can remember at once.
+		a.LastCards = a.LastCards[:a.MemoryLength]
+	}
+
+	return true
+
 }
