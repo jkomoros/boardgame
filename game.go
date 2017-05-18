jkomoros/boardgame

commit 4cfaa15330fee33f7508c07e065f67a8562b9ad4

When an agent proposes a move it waits some (small) variable amount of time to be more like a human, and not instant. Fixes #349.

game.go
@@ -617,7 +617,19 @@ func (g *Game) triggerAgents() error {
 		}
 
 		if move != nil {
-			g.ProposeMove(move, PlayerIndex(i))
+
+			//Slow down the playback of moves to more accurately emulate a human.
+
+			//TODO: if it's already been awhile since the last move was made
+			//(e.g. the agent was thinking for awhile), then apply
+			//immediately.
+
+			timeToWait := time.Duration(rand.Intn(int(2*time.Second))) + (500 * time.Millisecond)
+			player := i
+			go func() {
+				<-time.After(timeToWait)
+				g.ProposeMove(move, PlayerIndex(player))
+			}()
 		}
 	}
 	return nil

commit 936951df9b80d3c55715c8512c63ab91c00ba223

Fixed a (minor) test failure accidentally introduced in 4cfaa15.

game.go
@@ -46,6 +46,10 @@ type Game struct {
 	//Proposed moves is where moves that have been proposed but have not yet been applied go.
 	proposedMoves chan *proposedMoveItem
 
+	//if true, we will not wait to propose agent moves (mainly used for
+	//testing.)
+	instantAgentMoves bool
+
 	//Initalized is set to True after SetUp is called.
 	initalized bool
 
@@ -624,7 +628,11 @@ func (g *Game) triggerAgents() error {
 			//(e.g. the agent was thinking for awhile), then apply
 			//immediately.
 
-			g.delayedProposeMove(move, PlayerIndex(i), 500*time.Millisecond, 2*time.Second)
+			if g.instantAgentMoves {
+				g.ProposeMove(move, PlayerIndex(i))
+			} else {
+				g.delayedProposeMove(move, PlayerIndex(i), 500*time.Millisecond, 2*time.Second)
+			}
 		}
 	}
 	return nil

game_test.go
@@ -384,6 +384,8 @@ func TestIllegalPlayerIndex(t *testing.T) {
 func TestAgent(t *testing.T) {
 	game := testGame()
 
+	game.instantAgentMoves = true
+
 	assert.For(t).ThatActual(game.NumAgentPlayers()).Equals(0)
 
 	err := game.SetUp(3, []string{"", "Test", "Test"})