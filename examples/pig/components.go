958f2e65bac9379c82ccb766c25707c1c8da3cff
Alex Komoroske <jkomoros@gmail.com>
2017-06-15 05:12:42 -0700

    Implement and test Die.Roll(). Part of #372.

--- examples/pig/components.go
+++ examples/pig/components.go
@@ -1,5 +1,11 @@
 package pig
 
+import (
+	"errors"
+	"github.com/jkomoros/boardgame"
+	"math/rand"
+)
+
 const diceDeckName = "dice"
 
 //+autoreader
@@ -18,4 +24,26 @@ func DefaultDie() *dieValue {
 		Min: 1,
 		Max: 6,
 	}
+}
+
+//Roll sets the Value of the Die randomly to a new legal value. The component
+//you pass should be the same Die component that we're rolling.
+func (d *dieDynamicValue) Roll(c *boardgame.Component) error {
+
+	if c == nil {
+		return errors.New("No component provided")
+	}
+
+	values, ok := c.Values.(*dieValue)
+
+	if !ok {
+		return errors.New("Component passed was not a die")
+	}
+
+	random := rand.Intn(values.Max - values.Min)
+
+	d.Value = random + values.Min
+
+	return nil
+
 }
---
+++ examples/pig/components_test.go
@@ -0,0 +1,36 @@
+package pig
+
+import (
+	"github.com/jkomoros/boardgame"
+	"testing"
+)
+
+func TestDieRoll(t *testing.T) {
+
+	dynamic := &dieDynamicValue{
+		Value: 0,
+	}
+
+	values := DefaultDie()
+
+	die := &boardgame.Component{
+		Values: values,
+	}
+
+	seenValues := make(map[int]bool)
+
+	for i := 0; i < 10; i++ {
+		if err := dynamic.Roll(die); err != nil {
+			t.Fatal("Got non-nill err for roll: ", err)
+		}
+		if dynamic.Value < values.Min || dynamic.Value > values.Max {
+			t.Error("Invalid Value after roll: ", dynamic.Value)
+		}
+		seenValues[dynamic.Value] = true
+	}
+
+	if len(seenValues) < 3 {
+		t.Error("We didn't see enough different values across 10 rolls, which is suspicious.", len(seenValues))
+	}
+
+}
