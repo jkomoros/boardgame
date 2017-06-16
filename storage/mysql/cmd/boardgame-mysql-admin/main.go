ec9725c515f9a6d975f8efb01f10306edb4c8c52
Alex Komoroske <jkomoros@gmail.com>
2017-06-16 13:51:49 -0700

    Started working on flags for the db admin tool. Part of #273.

--- storage/mysql/cmd/boardgame-mysql-admin/main.go
+++ storage/mysql/cmd/boardgame-mysql-admin/main.go
@@ -4,9 +4,36 @@
 package main
 
 import (
+	"flag"
 	"log"
+	"os"
 )
 
+type appOptions struct {
+	Help    bool
+	flagSet *flag.FlagSet
+}
+
+func defineFlags(options *appOptions) {
+	options.flagSet.BoolVar(&options.Help, "help", false, "If true, will print help and exit.")
+}
+
+func getOptions(flagSet *flag.FlagSet, flagArguments []string) *appOptions {
+	options := &appOptions{flagSet: flagSet}
+	defineFlags(options)
+	flagSet.Parse(flagArguments)
+	return options
+}
+
 func main() {
+	flagSet := flag.CommandLine
+	process(getOptions(flagSet, os.Args[1:]))
+}
+
+func process(options *appOptions) {
+	if options.Help {
+		log.Println("You asked for help!")
+		return
+	}
 	log.Println("Hello world!")
 }
