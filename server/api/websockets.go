958ffa385c9bdbb2ec814577af781b5728540bbe
Alex Komoroske <jkomoros@gmail.com>
2017-06-21 07:21:05 -0700

    Inital stubs of versionNotifier, socket. Part of #401.

---
+++ server/api/websockets.go
@@ -0,0 +1,74 @@
+package api
+
+import (
+	"github.com/gorilla/websocket"
+)
+
+type gameVersionChanged struct {
+	Id      string
+	Version int
+}
+
+type versionNotifier struct {
+	sockets       map[string]map[*socket]bool
+	register      chan *socket
+	unregister    chan *socket
+	notifyVersion chan gameVersionChanged
+	doneChan      chan bool
+}
+
+type socket struct {
+	notifier *versionNotifier
+	conn     *websocket.Conn
+	send     chan []byte
+}
+
+func newSocket(conn *websocket.Conn, notifier *versionNotifier) *socket {
+	result := &socket{
+		notifier: notifier,
+		conn:     conn,
+		send:     make(chan []byte, 256),
+	}
+	go result.readPump()
+	go result.writePump()
+	return result
+}
+
+func (s *socket) readPump() {
+	//TODO: do work here
+}
+
+func (s *socket) writePump() {
+	//TODO: do work here
+}
+
+func newVersionNotifier() *versionNotifier {
+	result := &versionNotifier{
+		sockets:       make(map[string]map[*socket]bool),
+		register:      make(chan *socket),
+		unregister:    make(chan *socket),
+		notifyVersion: make(chan gameVersionChanged),
+		doneChan:      make(chan bool),
+	}
+	go result.workLoop()
+	return result
+}
+
+func (v *versionNotifier) done() {
+	close(v.doneChan)
+}
+
+func (v *versionNotifier) workLoop() {
+	for {
+		select {
+		case <-v.register:
+			//register s
+		case <-v.unregister:
+			//unregister s
+		case <-v.notifyVersion:
+			//Send message
+		case <-v.doneChan:
+			break
+		}
+	}
+}
