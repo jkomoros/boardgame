5e79d6dc7bc06bd54635aacae81859d996626ef1
Alex Komoroske <jkomoros@gmail.com>
2017-07-04 10:48:40 -0700

    Split out different moves into different files. Part of #56.

--- moves/currentplayer.go
+++ moves/currentplayer.go
@@ -1,57 +1,9 @@
-/*
-
-moves is a convenience package that implements composable Moves to make it
-easy to implement common logic. The Base move type is a very simple move that
-implements the basic stubs necessary for your straightforward moves to have
-minimal boilerplate.
-
-*/
 package moves
 
 import (
 	"errors"
 	"github.com/jkomoros/boardgame"
 )
-
-/*
-Base is an optional, convenience struct designed to be embedded
-anonymously in your own Moves. It implements no-op methods for many of the
-required methods on Moves. Legal and Apply are not covered, because every Move
-should implement their own, and if this implemented them it would obscure
-errors where for example your Legal() was incorrectly named and thus not used.
-In general your MoveConstructor can always be exactly the same, modulo the
-name of your underlying move type:
-
-	MoveConstructor: func() boardgame.Move {
- 		return new(myMoveStruct)
-	}
-
-Base cannot help your move implement PropertyReadSetter; use autoreader to
-generate that code for you.
-
-*/
-type Base struct {
-	moveType *boardgame.MoveType
-}
-
-func (d *Base) SetType(m *boardgame.MoveType) {
-	d.moveType = m
-}
-
-//Type simply returns BaseMove.MoveType
-func (d *Base) Type() *boardgame.MoveType {
-	return d.moveType
-}
-
-//DefaultsForState doesn't do anything
-func (d *Base) DefaultsForState(state boardgame.State) {
-	return
-}
-
-//Description defaults to returning the Type's HelpText()
-func (d *Base) Description() string {
-	return d.Type().HelpText()
-}
 
 /*
 
