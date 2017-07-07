fb0bedf0717fd63eddcf8e0b890ec25e8e2608a8
Alex Komoroske <jkomoros@gmail.com>
2017-07-07 16:59:39 -0700

    Add basic testing for enum. Part of #457.

---
+++ enum_test.go
@@ -0,0 +1,48 @@
+package boardgame
+
+import (
+	"github.com/workfit/tester/assert"
+	"testing"
+)
+
+func TestEnum(t *testing.T) {
+	enum := NewEnumManager()
+
+	const (
+		ColorBlue = iota
+		ColorGreen
+		ColorRed
+	)
+
+	const (
+		CardSpade = ColorRed + 1 + iota
+		CardClub
+		CardDiamond
+		CardHeart
+	)
+
+	const ConstDuplicate = iota
+
+	assert.For(t).ThatActual(enum).IsNotNil()
+
+	err := enum.Add("Color", ColorBlue, ColorGreen, ColorRed)
+
+	assert.For(t).ThatActual(err).IsNil()
+
+	assert.For(t).ThatActual(enum.Membership(ColorBlue)).Equals("Color")
+
+	err = enum.Add("Color", ColorBlue)
+
+	assert.For(t).ThatActual(err).IsNotNil()
+
+	err = enum.Add("Card", CardSpade, CardClub, CardDiamond, CardHeart)
+
+	assert.For(t).ThatActual(err).IsNil()
+
+	assert.For(t).ThatActual(enum.Membership(CardDiamond)).Equals("Card")
+
+	err = enum.Add("Another", ConstDuplicate)
+
+	assert.For(t).ThatActual(err).IsNotNil()
+
+}
