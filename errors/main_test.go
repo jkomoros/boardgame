ce4fb779d0869b27c20f086428545117cd946e9d
Alex Komoroske <jkomoros@gmail.com>
2017-07-18 06:21:06 -0700

    A few more tests of friendly errros. Part of #243.

--- errors/main_test.go
+++ errors/main_test.go
@@ -21,4 +21,22 @@ func TestFriendly(t *testing.T) {
 
 	assert.For(t).ThatActual(f.Fields()).Equals(Fields{})
 
+	friendlyTestMessage := "MY friendly message"
+
+	fields := Fields{"Foo": true}
+
+	friendly := f.WithFriendly(friendlyTestMessage, fields)
+
+	assert.For(t).ThatActual(friendly).IsNotNil()
+
+	assert.For(t).ThatActual(friendly).DoesNotEqual(f)
+
+	assert.For(t).ThatActual(friendly.FriendlyError()).Equals(friendlyTestMessage)
+
+	assert.For(t).ThatActual(friendly.Error()).Equals(testMessage)
+
+	assert.For(t).ThatActual(f.Fields()).Equals(Fields{})
+
+	assert.For(t).ThatActual(friendly.Fields()).Equals(fields)
+
 }
