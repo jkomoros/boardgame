b4427c19bcfa3249ef2d9d15f4b23da188950f74
Alex Komoroske <jkomoros@gmail.com>
2017-11-05 09:30:10 -0800

    A half-finished RoundRobin move is created. Currently doesn't have a way to know when the round robin is over. Part of #516.

--- moves/round_robin.go
+++ moves/round_robin.go
@@ -12,6 +12,15 @@ type RoundRobiner interface {
 	SetNextRoundRobinPlayer(nextPlayer boardgame.PlayerIndex)
 }
 
+//RoundRobinActioner should be implemented by any moves that embed a
+//RoundRobin move. It's the action that will be called on the player who is
+//next in the round robin.
+type RoundRobinActioner interface {
+	//RoundRobinAction should do the action for the round robin to the player
+	//in TargetPlayerIndex.
+	RoundRobinAction(state boardgame.MutableState) error
+}
+
 //StartRoundRobin is the move you should have in the progression immediately
 //before a round robin starts. It sets the NextRoundRobinPlayer to the game's
 //CurrentPlayer, getting ready for moves that embed RoundRobin.
@@ -40,4 +49,71 @@ func (s *StartRoundRobin) Apply(state boardgame.MutableState) error {
 	roundRobiner.SetNextRoundRobinPlayer(currentPlayer)
 
 	return nil
+}
+
+//RoundRobin is a type of move that goes around every player one by one and
+//does some action. Instead of using and modifying CurrentPlayerIndex, it uses
+//NextRoundRobinPlayer(). Other moves in this package embed RoundRobin. The
+//embeding move should implement RoundRobinActioner.
+type RoundRobin struct {
+	Base
+	TargetPlayerIndex boardgame.PlayerIndex
+}
+
+//AllowMultipleInProgression returns true because RoundRobins go until the end
+//condition of the round robin is met.
+func (r *RoundRobin) AllowMultipleInProgression() bool {
+	return true
+}
+
+//DefaultsForState sets the TargetPlayerIndex to NextRoundRobinPlayer.
+func (r *RoundRobin) DefaultsForState(state boardgame.State) {
+	roundRobiner, ok := state.GameState().(RoundRobiner)
+
+	if !ok {
+		return
+	}
+
+	r.TargetPlayerIndex = roundRobiner.NextRoundRobinPlayer()
+}
+
+func (r *RoundRobin) ValidConfiguration(exampleState boardgame.MutableState) error {
+	if _, ok := exampleState.GameState().(RoundRobiner); !ok {
+		return errors.New("GameState does not implement RoundRobiner interface")
+	}
+
+	embeddingMove := r.Info().Type().NewMove(exampleState)
+
+	if _, ok := embeddingMove.(RoundRobinActioner); !ok {
+		return errors.New("Embedding move doesn't implement RoundRobinActioner")
+	}
+	return nil
+}
+
+func (r *RoundRobin) Apply(state boardgame.MutableState) error {
+
+	//TODO: this is incorrect, because it doesn't handle the case where the
+	//configuration isn't set to defaults.
+	embeddingMove := r.Info().Type().NewMove(state)
+
+	actioner, ok := embeddingMove.(RoundRobinActioner)
+
+	if !ok {
+		return errors.New("Embedding move doesn't implement RoundRobinActioner")
+	}
+
+	if err := actioner.RoundRobinAction(state); err != nil {
+		return errors.New("RoundRobinAction returned error: " + err.Error())
+	}
+
+	roundRobiner, ok := state.GameState().(RoundRobiner)
+
+	if !ok {
+		return errors.New("GameState does not implement RoundRobiner interface")
+	}
+
+	roundRobiner.SetNextRoundRobinPlayer(r.TargetPlayerIndex.Next(state))
+
+	return nil
+
 }
