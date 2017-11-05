2e74f8a0017b240280909a57cbc5f232c953ef5d
Alex Komoroske <jkomoros@gmail.com>
2017-11-05 11:48:14 -0800

    Define a DealComponents move type that subclasses RoundRobin. Part of #516.

---
+++ moves/deal_components.go
@@ -0,0 +1,81 @@
+package moves
+
+import (
+	"errors"
+	"github.com/jkomoros/boardgame"
+)
+
+//PlayerStacker should be implemented by your embedding Move if you embed
+//DealComponents. It will be consulted to figure out where the PlayerStack is
+//to deal a component to.
+type PlayerStacker interface {
+	PlayerStack(playerState boardgame.MutablePlayerState) boardgame.MutableStack
+}
+
+//GameStacker should be implemented by your emedding Move if you embed
+//DealComponents. It will be consulted to figure out where to draw the
+//components from to deal to players.
+type GameStacker interface {
+	GameStack(gameState boardgame.MutableSubState) boardgame.MutableStack
+}
+
+/*
+
+DealComponents is a type of RoundRobin move that deals components from
+gameState's GameStack() to each PlayerState's PlayerStack(). By default it
+goes around once and deals a single component. If you want different end
+conditions, override RoundRobinFinished() on your move.
+
+For example, if you want to deal two cards to each player, set your
+RoundRobinFinished like so:
+
+	func (m *MyMove) RoundRobinFinished(state boardgame.State) error {
+		return m.RoundRobinFinishedMultiCircuit(2, state)
+	}
+
+*/
+type DealComponents struct {
+	RoundRobin
+}
+
+func (d *DealComponents) ValidConfiguration(exampleState boardgame.MutableState) error {
+	if _, ok := d.TopLevelStruct().(PlayerStacker); !ok {
+		return errors.New("Embedding move doesn't implement PlayerStacker")
+	}
+
+	if _, ok := d.TopLevelStruct().(GameStacker); !ok {
+		return errors.New("Embedding move doesn't implement GameStacker")
+	}
+
+	return d.RoundRobin.ValidConfiguration(exampleState)
+}
+
+func (d *DealComponents) RoundRobinAction(playerState boardgame.MutablePlayerState) error {
+
+	playerStacker, ok := d.TopLevelStruct().(PlayerStacker)
+
+	if !ok {
+		return errors.New("Embedding move unexpectedly doesn't implement PlayerStacker")
+	}
+
+	targetStack := playerStacker.PlayerStack(playerState)
+
+	if targetStack == nil {
+		return errors.New("PlayerStacker didn't return a valid stack")
+	}
+
+	gameStacker, ok := d.TopLevelStruct().(GameStacker)
+
+	if !ok {
+		return errors.New("Embedding move unexpectedly doesn't implement GameStacker")
+	}
+
+	sourceStack := gameStacker.GameStack(playerState.MutableState().MutableGameState())
+
+	if sourceStack == nil {
+		return errors.New("GameStacker didn't return a valid stack")
+	}
+
+	return sourceStack.MoveComponent(boardgame.FirstComponentIndex, targetStack, boardgame.NextSlotIndex)
+
+}
