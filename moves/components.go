4616bda23516bed0a3ed767bc0a1529884a5aba7
Alex Komoroske <jkomoros@gmail.com>
2017-11-05 13:39:12 -0800

    Implement a starter CollectComponents move. Part of #516.

--- moves/components.go
+++ moves/components.go
@@ -79,3 +79,65 @@ func (d *DealComponents) RoundRobinAction(playerState boardgame.MutablePlayerSta
 	return sourceStack.MoveComponent(boardgame.FirstComponentIndex, targetStack, boardgame.NextSlotIndex)
 
 }
+
+/*
+
+CollectComponents is a type of RoundRobin move that collects components from
+each PlayerState's PlayerStack() to gameState's GameStack(). By default it
+goes around once and collects a component from each. If you want different end
+conditions, override RoundRobinFinished() on your move.
+
+For example, if you want to collect two cards from each player, set your
+RoundRobinFinished like so:
+
+	func (m *MyMove) RoundRobinFinished(state boardgame.State) error {
+		return m.RoundRobinFinishedMultiCircuit(2, state)
+	}
+
+
+*/
+type CollectComponents struct {
+	RoundRobin
+}
+
+func (d *CollectComponents) ValidConfiguration(exampleState boardgame.MutableState) error {
+	if _, ok := d.TopLevelStruct().(PlayerStacker); !ok {
+		return errors.New("Embedding move doesn't implement PlayerStacker")
+	}
+
+	if _, ok := d.TopLevelStruct().(GameStacker); !ok {
+		return errors.New("Embedding move doesn't implement GameStacker")
+	}
+
+	return d.RoundRobin.ValidConfiguration(exampleState)
+}
+
+func (d *CollectComponents) RoundRobinAction(playerState boardgame.MutablePlayerState) error {
+
+	playerStacker, ok := d.TopLevelStruct().(PlayerStacker)
+
+	if !ok {
+		return errors.New("Embedding move unexpectedly doesn't implement PlayerStacker")
+	}
+
+	playerStack := playerStacker.PlayerStack(playerState)
+
+	if playerStack == nil {
+		return errors.New("PlayerStacker didn't return a valid stack")
+	}
+
+	gameStacker, ok := d.TopLevelStruct().(GameStacker)
+
+	if !ok {
+		return errors.New("Embedding move unexpectedly doesn't implement GameStacker")
+	}
+
+	targetStack := gameStacker.GameStack(playerState.MutableState().MutableGameState())
+
+	if targetStack == nil {
+		return errors.New("GameStacker didn't return a valid stack")
+	}
+
+	return playerStack.MoveComponent(boardgame.FirstComponentIndex, targetStack, boardgame.NextSlotIndex)
+
+}
