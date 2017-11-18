d2c3e23ffceb3442bcd83d32639c7959242c94a1
Alex Komoroske <jkomoros@gmail.com>
2017-11-18 08:57:15 -0800

    MoveCountComponents is now the base class for the other Move*Components, which is cleaner and seems more sane. Part of #516.

--- moves/move_components.go
+++ moves/move_components.go
@@ -11,18 +11,19 @@ type sourceDestinationStacker interface {
 	moveinterfaces.DestinationStacker
 }
 
-//MoveComponentsUntilCountReached is a move that will move components, one at
-//a time, from SourceStack() to DestinationStack() until the target stack is
-//up to having TargetCount components in it. Other MoveComponents-style moves
-//derive from this. When using it you must implement
-//moveinterfaces.SourceStacker and moveinterfaces.DestinationStacker to encode
-//which stacks to use. You may also want to override TargetCount() if you want
-//to move more than one component.
-type MoveComponentsUntilCountReached struct {
-	ApplyUntilCount
+//MoveCountComponents is a move that will move components, one at a time, from
+//SourceStack() to DestinationStack() until TargetCount() components have been
+//moved. It is like DealComponents or CollectComponnets, except instead of
+//working on a certain stack for each player, it operates on two fixed stacks.
+//Other MoveComponents-style moves derive from this. When using it you must
+//implement moveinterfaces.SourceStacker and moveinterfaces.DestinationStacker
+//to encode which stacks to use. You may also want to override TargetCount()
+//if you want to move more than one component.
+type MoveCountComponents struct {
+	ApplyCountTimes
 }
 
-func (m *MoveComponentsUntilCountReached) ValidConfiguration(exampleState boardgame.MutableState) error {
+func (m *MoveCountComponents) ValidConfiguration(exampleState boardgame.MutableState) error {
 	if err := m.ApplyUntilCount.ValidConfiguration(exampleState); err != nil {
 		return err
 	}
@@ -34,14 +35,8 @@ func (m *MoveComponentsUntilCountReached) ValidConfiguration(exampleState boardg
 	return nil
 }
 
-//CountDown returns false, because as we move components from source to
-//destination, destination will be getting larger.
-func (m *MoveComponentsUntilCountReached) CountDown(state boardgame.State) bool {
-	return false
-}
-
 //stacks returns the source and desitnation so you don't have to do the cast.
-func (m *MoveComponentsUntilCountReached) stacks(state boardgame.State) (source, destination boardgame.MutableStack) {
+func (m *MoveCountComponents) stacks(state boardgame.State) (source, destination boardgame.MutableStack) {
 
 	//TODO: this is a total hack
 	mState := state.(boardgame.MutableState)
@@ -56,21 +51,9 @@ func (m *MoveComponentsUntilCountReached) stacks(state boardgame.State) (source,
 
 }
 
-//Count returns the number of components in DestinationStack().
-func (m *MoveComponentsUntilCountReached) Count(state boardgame.State) int {
-
-	_, targetStack := m.stacks(state)
-
-	if targetStack == nil {
-		return 0
-	}
-
-	return targetStack.NumComponents()
-}
-
 //Apply by default moves one component from SourceStack() to
 //DestinationStack(). You likely do not need to override this method.
-func (m *MoveComponentsUntilCountReached) Apply(state boardgame.MutableState) error {
+func (m *MoveCountComponents) Apply(state boardgame.MutableState) error {
 
 	source, destination := m.stacks(state)
 
@@ -86,13 +69,39 @@ func (m *MoveComponentsUntilCountReached) Apply(state boardgame.MutableState) er
 
 }
 
+//MoveComponentsUntilCountReached is a move that will move components, one at
+//a time, from SourceStack() to DestinationStack() until the target stack is
+//up to having TargetCount components in it. See also
+//MoveComponentsUntilCountLeft for a slightly different end condition.
+type MoveComponentsUntilCountReached struct {
+	MoveCountComponents
+}
+
+//CountDown returns false, because as we move components from source to
+//destination, destination will be getting larger.
+func (m *MoveComponentsUntilCountReached) CountDown(state boardgame.State) bool {
+	return false
+}
+
+//Count returns the number of components in DestinationStack().
+func (m *MoveComponentsUntilCountReached) Count(state boardgame.State) int {
+
+	_, targetStack := m.stacks(state)
+
+	if targetStack == nil {
+		return 0
+	}
+
+	return targetStack.NumComponents()
+}
+
 //MoveComponentsUntilCountLeft is a move that will move components, one at a
 //time, from SourceStack() to DestinationStack() until the source stack is
-//down to having  TargetCount components in it. It subclasses
-//MoveComponentsUntilCountReached, and its primary difference is that its
-//target is based on reducing the size of SourceStack to a target size.
+//down to having  TargetCount components in it. Its primary difference from
+//MoveComponentsUntilCountReached is that its target is based on reducing the
+//size of SourceStack to a target size.
 type MoveComponentsUntilCountLeft struct {
-	MoveComponentsUntilCountReached
+	MoveCountComponents
 }
 
 //Count returns the number of components in the SourceStack().
@@ -110,17 +119,4 @@ func (m *MoveComponentsUntilCountLeft) Count(state boardgame.State) int {
 //destination, source will be getting smaller and smaller.
 func (m *MoveComponentsUntilCountLeft) CountDown(state boardgame.State) bool {
 	return true
-}
-
-//MoveComponents is a move that will move components, one at a time, from
-//SourceStack() to DestinationStack() until TargetCount() components have been
-//moved. It is like DealComponents or CollectComponnets, except instead of
-//working on a certain stack for each player, it operates on two fixed stacks.
-type MoveCountComponents struct {
-	MoveComponentsUntilCountReached
-}
-
-//Count counts the number of times this move has been applied in a row.
-func (m *MoveCountComponents) Count(state boardgame.State) int {
-	return countMovesApplied(m.TopLevelStruct(), state)
 }
