975c8a921a53801a9ba8411702100107339c770b
Alex Komoroske <jkomoros@gmail.com>
2017-11-21 11:53:41 -0800

    Syncornous config.js script to load up API HOst. Not used for anything. Part of #231.

---
+++ server/static/webapp/config.js
@@ -0,0 +1,14 @@
+var CONFIG = {
+	"firebase" :{
+		"apiKey": "AIzaSyDi0hhBgLPbpJgprVCDzDkk8zuFpb9XadM",
+		"authDomain": "example-boardgame.firebaseapp.com",
+		"databaseURL": "https://example-boardgame.firebaseio.com",
+		"projectId": "example-boardgame",
+		"storageBucket": "example-boardgame.appspot.com",
+		"messagingSenderId": "138149526364"
+	},
+	"api": {
+		"dev_host": "http://localhost:8888",
+		"host": "https://boardgame-159316.appspot.com"
+	}
+}
