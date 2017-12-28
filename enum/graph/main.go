faa497d9053d2e30ff1f0b148f9e371bc9008305
Alex Komoroske <jkomoros@gmail.com>
2017-12-28 15:41:06 -0500

    Implement most of the underlying graph struct methods. New() still returns nil. Part of #493.

--- enum/graph/main.go
+++ enum/graph/main.go
@@ -9,16 +9,22 @@
 package graph
 
 import (
+	"errors"
 	"github.com/jkomoros/boardgame/enum"
+	"strconv"
 )
 
 type Graph interface {
+	//AddEdge adds the edge to the graph if it doesn't exist, and if the graph
+	//isn't finished yet. Will error if from or to aren't in the given enum.
 	AddEdge(from, to int) error
 	Connected(from, to int) bool
 	Neighbors(start int) []int
 
 	//Defaults to 0 for edges that haven't had SetEdgeWeight called.
 	EdgeWeight(from, to int) int
+	//SetEdgeWeight sets the weight between the two nodes. Errors if the graph
+	//is already finished, or if those two nodes aren't connected.
 	SetEdgeWeight(from, to int, weight int) error
 
 	//After finish is called, no modifications may be made to the graph.
@@ -26,11 +32,88 @@ type Graph interface {
 }
 
 type graph struct {
-	directed    bool
+	undirected  bool
+	finished    bool
+	theEnum     enum.Enum
 	edges       map[int]map[int]bool
 	edgeWeights map[string]int
 }
 
-func New(directed bool, enum enum.Enum) Graph {
+func New(undirected bool, enum enum.Enum) Graph {
+	return nil
+}
+
+func (g *graph) Finish() {
+	g.finished = true
+}
+
+func (g *graph) AddEdge(from, to int) error {
+	if err := g.addEdgeImpl(from, to); err != nil {
+		return err
+	}
+	if g.undirected {
+		return g.addEdgeImpl(to, from)
+	}
+	return nil
+}
+
+func (g *graph) addEdgeImpl(from, to int) error {
+	if !g.theEnum.Valid(from) {
+		return errors.New("from value is not legal in that enum")
+	}
+	if !g.theEnum.Valid(to) {
+		return errors.New("to value is not legal in that enum")
+	}
+	if g.finished {
+		return errors.New("graph is finished so no modifications may be made")
+	}
+	edgeMap := g.edges[from]
+	if edgeMap == nil {
+		edgeMap = make(map[int]bool)
+		g.edges[from] = edgeMap
+	}
+	edgeMap[to] = true
+	return nil
+}
+
+func (g *graph) Connected(from, to int) bool {
+	edgeMap := g.edges[from]
+	if edgeMap == nil {
+		return false
+	}
+	return edgeMap[to]
+}
+
+func (g *graph) Neighbors(start int) []int {
+	edgeMap := g.edges[start]
+	if edgeMap == nil {
+		return nil
+	}
+	result := make([]int, len(edgeMap))
+	counter := 0
+	for key, _ := range edgeMap {
+		result[counter] = key
+		counter++
+	}
+	return result
+}
+
+func keyForEdge(from, to int) string {
+	return strconv.Itoa(from) + "-" + strconv.Itoa(to)
+}
+
+func (g *graph) EdgeWeight(from, to int) int {
+	//If the edge doesn't exist, the default of 0 is fine
+	return g.edgeWeights[keyForEdge(from, to)]
+}
+
+func (g *graph) SetEdgeWeight(from, to int, weight int) error {
+	if !g.Connected(from, to) {
+		return errors.New("from and to do not share an edge")
+	}
+	if g.finished {
+		return errors.New("graph is finished so no modifications may be made")
+	}
+	g.edgeWeights[keyForEdge(from, to)] = weight
 	return nil
 }
