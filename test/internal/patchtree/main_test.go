b4370a39380efee9891d2e94f542805165232dde
Alex Komoroske <jkomoros@gmail.com>
2018-04-22 09:28:59 -0700

    Add initial tests for patchtree, which demonstrate it fails. Part of #607.

---
+++ test/internal/patchtree/main_test.go
@@ -0,0 +1,40 @@
+package patchtree
+
+import (
+	jd "github.com/jkomoros/jd/lib"
+	"github.com/workfit/tester/assert"
+	"testing"
+)
+
+func TestBasic(t *testing.T) {
+	blob, err := JSON("test/a/b")
+
+	assert.For(t).ThatActual(err).IsNil()
+
+	compareBlobTo(t, blob, "expected_a_b.json")
+}
+
+func compareBlobTo(t *testing.T, blob []byte, fileName string) {
+
+	node, err := jd.ReadJsonString(string(blob))
+
+	if err != nil {
+		t.Error("Got error reading composed blob: " + err.Error())
+		return
+	}
+
+	fileName = "./test/" + fileName
+
+	expected, err := jd.ReadJsonFile(fileName)
+
+	if err != nil {
+		t.Error("Got error loading expected: " + err.Error())
+		return
+	}
+
+	if !node.Equals(expected) {
+		diff := node.Diff(expected)
+		t.Error("Nodes don't match: " + diff.Render())
+		return
+	}
+}
