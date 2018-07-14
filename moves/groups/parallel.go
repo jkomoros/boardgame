a35123b2b86db187177cd79dd5f8a8e5df089bb3
Alex Komoroske <jkomoros@gmail.com>
2018-07-14 12:53:48 +0200

    Split parallel implementation into separate file for clarity. Part of #627.

--- moves/groups/parallel.go
+++ moves/groups/parallel.go
@@ -1,10 +1,3 @@
-/*
-
-	groups is a package of various objects that implement
-	moves/interfaces.MoveProgressionGroup, and are thus appropriate for
-	passing to moves.AddOrderedForPhase.
-
-*/
 package groups
 
 import (
@@ -15,42 +8,6 @@ import (
 	"math"
 	"sort"
 )
-
-//Serial is a type of move group that represents the sub-groups provided from top
-//to bottom, in order. It is one of the most basic types of groups.
-type Serial []interfaces.MoveProgressionGroup
-
-func (s Serial) MoveConfigs() []boardgame.MoveConfig {
-	var result []boardgame.MoveConfig
-	for _, group := range s {
-		result = append(result, group.MoveConfigs()...)
-	}
-	return result
-}
-
-//Satisfied walks through each sub-group in turn. It errors if no tape is read.
-func (s Serial) Satisfied(tape *interfaces.MoveGroupHistoryItem) (error, *interfaces.MoveGroupHistoryItem) {
-
-	tapeHead := tape
-
-	for _, group := range s {
-
-		if tapeHead == nil {
-			return nil, nil
-		}
-
-		err, rest := group.Satisfied(tapeHead)
-		if err != nil {
-			return err, tape
-		}
-
-		tapeHead = rest
-
-	}
-
-	return nil, tapeHead
-
-}
 
 //Parallel is a type of move group that requires all sub-groups to be present,
 //but in any order. It is one of the most basic types of groups. If you want
