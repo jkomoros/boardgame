0bb4dba4f6c5c28452f307cbf823adb9d78126f9
Alex Komoroske <jkomoros@gmail.com>
2018-07-23 09:18:55 +0200

    Add tests for filesystem. Now filesystem passes all of the main tests for a storage manager. Part of #648.

---
+++ storage/filesystem/main_test.go
@@ -0,0 +1,14 @@
+package filesystem
+
+import (
+	"github.com/jkomoros/boardgame/storage/internal/test"
+	"testing"
+)
+
+func TestStorageManager(t *testing.T) {
+
+	test.Test(func() test.StorageManager {
+		return NewStorageManager("test")
+	}, "filesystem", "", t)
+
+}
