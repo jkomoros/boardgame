f65670d4b0c6b319b38b47a7d8a89629f98fe49a
Alex Komoroske <jkomoros@gmail.com>
2018-07-26 17:35:54 -0700

    Factor the logic behind magic folder creation in filesystem to the util/golden library, where it makes much more sense. No behavior change, just refactoring and changing where documentation is. Part of #648.

--- storage/filesystem/main.go
+++ storage/filesystem/main.go
@@ -18,21 +18,17 @@
 package filesystem
 
 import (
-	"bytes"
 	"errors"
 	"github.com/jkomoros/boardgame"
 	"github.com/jkomoros/boardgame/server/api/extendedgame"
 	"github.com/jkomoros/boardgame/server/api/listing"
 	"github.com/jkomoros/boardgame/storage/filesystem/record"
 	"github.com/jkomoros/boardgame/storage/internal/helpers"
-	"go/ast"
-	"go/parser"
-	"go/token"
+	"github.com/jkomoros/boardgame/util/golden"
 	"io/ioutil"
 	"log"
 	"os"
 	"path/filepath"
-	"reflect"
 	"strings"
 )
 
@@ -53,14 +49,9 @@ func init() {
 
 //NewStorageManager returns a new filesystem storage manager. basePath is the
 //folder, relative to this executable, to have as the root of the storage
-//pool. If goldenFolderName is not "", then we will use reflection to find the
-//package path for each delegate, ensure a folder exists within it with that
-//name, create a soft-link from basePath to that folder, and create a
-//`golden_test.go` file that automatically tests all of those golden files
-//(and assumes that your package defines a `NewDelegate()
-//boardgame.GameDelegate` method). The result is that the underlying files
-//will be stored in folders adjacent to the games they are relative to, which
-//is convenient if you're adding new golden games to the test set.
+//pool. If goldenFolderName is not "", we will call golden.LinkGoldenFolders
+//with that folder name and all of the managers bassed in WithManagers. See
+//util/golden.LinkGoldenFolders for more.
 func NewStorageManager(basePath string, goldenFolderName string) *StorageManager {
 
 	result := &StorageManager{
@@ -86,159 +77,13 @@ func (s *StorageManager) Connect(config string) error {
 	}
 
 	if s.goldenFolderName != "" {
-
 		log.Println("Creating and linking golden folders")
-		for _, manager := range s.managers {
-			pkgPath := reflect.ValueOf(manager.Delegate()).Elem().Type().PkgPath()
-			if err := s.LinkGoldenFolder(manager.Delegate().Name(), pkgPath); err != nil {
-				return errors.New("Couldn't link golden folder for " + manager.Delegate().Name() + ": " + err.Error())
-			}
+		if err := golden.LinkGoldenFolders(s.basePath, s.goldenFolderName, s.managers); err != nil {
+			return errors.New("Couldn't link golden folders: " + err.Error())
 		}
 	}
 
 	return nil
-}
-
-func (s *StorageManager) LinkGoldenFolder(gameType, pkgPath string) error {
-	//TODO: does this handle vendoring correctly?
-
-	if s.goldenFolderName == "" {
-		return nil
-	}
-
-	goPath := os.Getenv("GOPATH")
-
-	if goPath == "" {
-		return errors.New("Gopath wasn't set")
-	}
-
-	fullPkgPath := filepath.Join(goPath, "src", pkgPath)
-
-	fullPath := filepath.Join(fullPkgPath, s.goldenFolderName)
-
-	if _, err := os.Stat(fullPath); os.IsNotExist(err) {
-		log.Println("Creating " + fullPath)
-		if err := os.Mkdir(fullPath, 0700); err != nil {
-			return errors.New("Could not make golden path: " + err.Error())
-		}
-	}
-
-	gamePath := filepath.Join(s.basePath, gameType)
-
-	if _, err := os.Stat(gamePath); os.IsNotExist(err) {
-		log.Println("Linking " + gamePath + " to " + fullPath)
-		//Soft link from basePath.
-		if err := os.Symlink(fullPath, gamePath); err != nil {
-			return errors.New("Couldn't create symlink: " + err.Error())
-		}
-	}
-
-	//TODO: ok, this is getting weird that the filesystem storage layer is now
-	//a full-fledged util.
-
-	//TODO: allow this to be skipped as an otpion.
-	if err := s.SaveGoldenTest(fullPkgPath); err != nil {
-		return errors.New("Couldn't store golden test: " + err.Error())
-	}
-
-	return nil
-
-}
-
-func (s *StorageManager) SaveGoldenTest(fullPkgPath string) error {
-
-	pkgName, err := verifyPkgForGolden(fullPkgPath)
-
-	if err != nil {
-		return errors.New("Package didn't validate: " + err.Error())
-	}
-
-	buf := new(bytes.Buffer)
-
-	err = goldenTestTemplate.Execute(buf, map[string]string{
-		"gametype": pkgName,
-		"folder":   s.goldenFolderName,
-	})
-
-	if err != nil {
-		return errors.New("Couldn't generate blob from template: " + err.Error())
-	}
-
-	return ioutil.WriteFile(filepath.Join(fullPkgPath, "golden_test.go"), buf.Bytes(), 0644)
-
-}
-
-//verifyPkgForGolden looks at the given package, returns the package name, and
-//verifies that it has a NewDelegate method.
-func verifyPkgForGolden(fullPkgName string) (string, error) {
-	pkgs, err := parser.ParseDir(token.NewFileSet(), fullPkgName, nil, 0)
-
-	if err != nil {
-		return "", errors.New("Couldn't parse folder: " + err.Error())
-	}
-
-	if len(pkgs) < 1 {
-		return "", errors.New("No packages in that directory")
-	}
-
-	if len(pkgs) > 1 {
-		return "", errors.New("More than one package in that directory")
-	}
-
-	var pkg *ast.Package
-	pkgName := ""
-
-	for key, p := range pkgs {
-		pkgName = key
-		pkg = p
-	}
-
-	foundNewDelegate := false
-
-	for _, file := range pkg.Files {
-		for _, decl := range file.Decls {
-			fun, ok := decl.(*ast.FuncDecl)
-			if !ok {
-				continue
-			}
-
-			if fun.Name.String() != "NewDelegate" {
-				continue
-			}
-
-			//OK, it might be the function. Does it have the right signature?
-
-			if fun.Recv != nil {
-				return "", errors.New("NewDelegate had a receiver")
-			}
-
-			if fun.Type.Params.NumFields() > 0 {
-				return "", errors.New("NewDelegate took more than 0 items")
-			}
-
-			if fun.Type.Results.NumFields() != 1 {
-				return "", errors.New("NewDelegate didn't return exactly one item")
-			}
-
-			//TODO: check that the returned item implements
-			//boardgame.GameDelegate.
-
-			foundNewDelegate = true
-			break
-
-		}
-
-		if foundNewDelegate {
-			break
-		}
-	}
-
-	if !foundNewDelegate {
-		return "", errors.New("Couldn't find NewDelegate")
-	}
-
-	return pkgName, nil
-
 }
 
 func (s *StorageManager) WithManagers(managers []*boardgame.GameManager) {
---
+++ util/golden/link.go
@@ -0,0 +1,219 @@
+package golden
+
+import (
+	"bytes"
+	"errors"
+	"github.com/jkomoros/boardgame"
+	"go/ast"
+	"go/parser"
+	"go/token"
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+	"reflect"
+	"text/template"
+)
+
+var goldenTestTemplate *template.Template
+
+func init() {
+	goldenTestTemplate = template.Must(template.New("golden").Parse(goldenTestTemplateText))
+}
+
+//LinkGoldenFolders helps create a folder system for the `filesystem` storage
+//layer, with the individual folders pointing back to folders adjacent to the
+//games they're affiliated with. This is useful when you want to generate new
+//golden tests for game types. First we use reflection to find the package
+//path for each delegate, ensure a folder exists within it with tbe
+//goldenFolderName name, create a soft-link from basePath to that folder, and
+//create a `golden_test.go` file that automatically tests all of those golden
+//files (and assumes that your package defines a `NewDelegate()
+//boardgame.GameDelegate` method). The result is that the underlying files
+//will be stored in folders adjacent to the games they are relative to, which
+//is convenient if you're adding new golden games to the test set. The
+//`filesystem` storage layer will call this as a convenience if you pass a
+//non-"" goldenFolderName to filesystem.NewStorageManager.
+func LinkGoldenFolders(basePath string, goldenFolderName string, managers []*boardgame.GameManager) error {
+
+	if goldenFolderName == "" {
+		goldenFolderName = "golden"
+	}
+
+	if _, err := os.Stat(basePath); os.IsNotExist(err) {
+		return errors.New(basePath + " does not already exist.")
+	}
+
+	for _, manager := range managers {
+		pkgPath := reflect.ValueOf(manager.Delegate()).Elem().Type().PkgPath()
+		if err := linkGoldenFolder(manager.Delegate().Name(), pkgPath, basePath, goldenFolderName); err != nil {
+			return errors.New("Couldn't link golden folder for " + manager.Delegate().Name() + ": " + err.Error())
+		}
+	}
+
+	return nil
+}
+
+func linkGoldenFolder(gameType, pkgPath, basePath, goldenFolderName string) error {
+
+	//TODO: should this be public?
+
+	//TODO: does this handle vendoring correctly?
+
+	goPath := os.Getenv("GOPATH")
+
+	if goPath == "" {
+		return errors.New("Gopath wasn't set")
+	}
+
+	fullPkgPath := filepath.Join(goPath, "src", pkgPath)
+
+	fullPath := filepath.Join(fullPkgPath, goldenFolderName)
+
+	if _, err := os.Stat(fullPath); os.IsNotExist(err) {
+		log.Println("Creating " + fullPath)
+		if err := os.Mkdir(fullPath, 0700); err != nil {
+			return errors.New("Could not make golden path: " + err.Error())
+		}
+	}
+
+	gamePath := filepath.Join(basePath, gameType)
+
+	if _, err := os.Stat(gamePath); os.IsNotExist(err) {
+		log.Println("Linking " + gamePath + " to " + fullPath)
+		//Soft link from basePath.
+		if err := os.Symlink(fullPath, gamePath); err != nil {
+			return errors.New("Couldn't create symlink: " + err.Error())
+		}
+	}
+
+	//TODO: allow this to be skipped as an otpion.
+	if err := createGoldenTest(fullPkgPath, goldenFolderName); err != nil {
+		return errors.New("Couldn't store golden test: " + err.Error())
+	}
+
+	return nil
+
+}
+
+func createGoldenTest(fullPkgPath, goldenFolderName string) error {
+
+	//TODO: should this be public?
+
+	pkgName, err := verifyPkgForGolden(fullPkgPath)
+
+	if err != nil {
+		return errors.New("Package didn't validate: " + err.Error())
+	}
+
+	buf := new(bytes.Buffer)
+
+	err = goldenTestTemplate.Execute(buf, map[string]string{
+		"gametype": pkgName,
+		"folder":   goldenFolderName,
+	})
+
+	if err != nil {
+		return errors.New("Couldn't generate blob from template: " + err.Error())
+	}
+
+	return ioutil.WriteFile(filepath.Join(fullPkgPath, "golden_test.go"), buf.Bytes(), 0644)
+
+}
+
+//verifyPkgForGolden looks at the given package, returns the package name, and
+//verifies that it has a NewDelegate method.
+func verifyPkgForGolden(fullPkgName string) (string, error) {
+	pkgs, err := parser.ParseDir(token.NewFileSet(), fullPkgName, nil, 0)
+
+	if err != nil {
+		return "", errors.New("Couldn't parse folder: " + err.Error())
+	}
+
+	if len(pkgs) < 1 {
+		return "", errors.New("No packages in that directory")
+	}
+
+	if len(pkgs) > 1 {
+		return "", errors.New("More than one package in that directory")
+	}
+
+	var pkg *ast.Package
+	pkgName := ""
+
+	for key, p := range pkgs {
+		pkgName = key
+		pkg = p
+	}
+
+	foundNewDelegate := false
+
+	for _, file := range pkg.Files {
+		for _, decl := range file.Decls {
+			fun, ok := decl.(*ast.FuncDecl)
+			if !ok {
+				continue
+			}
+
+			if fun.Name.String() != "NewDelegate" {
+				continue
+			}
+
+			//OK, it might be the function. Does it have the right signature?
+
+			if fun.Recv != nil {
+				return "", errors.New("NewDelegate had a receiver")
+			}
+
+			if fun.Type.Params.NumFields() > 0 {
+				return "", errors.New("NewDelegate took more than 0 items")
+			}
+
+			if fun.Type.Results.NumFields() != 1 {
+				return "", errors.New("NewDelegate didn't return exactly one item")
+			}
+
+			//TODO: check that the returned item implements
+			//boardgame.GameDelegate.
+
+			foundNewDelegate = true
+			break
+
+		}
+
+		if foundNewDelegate {
+			break
+		}
+	}
+
+	if !foundNewDelegate {
+		return "", errors.New("Couldn't find NewDelegate")
+	}
+
+	return pkgName, nil
+
+}
+
+var goldenTestTemplateText = `package {{.gametype}}
+
+/*
+
+This file was created automatically by the filesystem storage layer with a
+golden folder.
+
+It will be overwritten the next time a filesystem is booted that uses this
+game package.
+
+*/
+
+import (
+	"github.com/jkomoros/boardgame/util/golden"
+	"testing"
+)
+
+func TestGolden(t *testing.T) {
+	if err := golden.CompareFolder(NewDelegate(), "{{.folder}}"); err != nil {
+		t.Error(err.Error())
+	}
+}
+`
