883affa1808209225cf05659a75dc93b5895640f
Alex Komoroske <jkomoros@gmail.com>
2018-08-05 12:23:25 -0700

    Split out helpers out of base_cmd. Part of #655.

--- boardgame-util/base_cmd.go
+++ boardgame-util/base_cmd.go
@@ -5,41 +5,6 @@ import (
 	"github.com/bobziuchkovski/writ"
 )
 
-//SubcommandObject is a literal struct that implements a subcommand
-type SubcommandObject interface {
-	//The name the command is registered as
-	Name() string
-	//The description of the command to register
-	Description() string
-	//The aliases to register for
-	Aliases() []string
-	//The rest of the usage string, which will be appened to "NAME "
-	Usage() string
-	//SubcommandObjects should return the list of sub comamnds, or nil if a
-	//terminal command.
-	SubcommandObjects() []SubcommandObject
-	//The command to actually run
-	Run(p writ.Path, positional []string)
-}
-
-type baseSubCommand struct{}
-
-func (b *baseSubCommand) Aliases() []string {
-	return nil
-}
-
-func (b *baseSubCommand) Description() string {
-	return ""
-}
-
-func (b *baseSubCommand) Usage() string {
-	return ""
-}
-
-func (b *baseSubCommand) SubcommandObjects() []SubcommandObject {
-	return nil
-}
-
 type BoardgameUtil struct {
 	baseSubCommand
 	Help Help
