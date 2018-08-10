e8c321cba8ca9d640d9d1a5a25de52a4bee76ffb
Alex Komoroske <jkomoros@gmail.com>
2018-08-09 19:58:12 -0700

    Start fleshing out a bit more of the shape of the Api method in the build library. Part of #655.

--- boardgame-util/lib/build/main.go
+++ boardgame-util/lib/build/main.go
@@ -1,5 +1,60 @@
 /*
 
+build is a package that can create and cleanup api server binaries, static
+asset folders, and golden test setups.
 
- */
+Typically it is not used directly, but via the `boardgame-util build` and
+`boardgame-util cleanup` commands.
+
+*/
 package build
+
+import (
+	"errors"
+	"os"
+	"strings"
+)
+
+type StorageType int
+
+const (
+	StorageInvalid StorageType = iota
+	StorageMemory
+	StorageBolt
+	StorageMysql
+	StorageFilesystem
+)
+
+func StorageTypeFromString(in string) StorageType {
+	in = strings.ToLower(in)
+	in = strings.TrimSpace(in)
+
+	switch in {
+	case "memory":
+		return StorageMemory
+	case "bolt":
+		return StorageBolt
+	case "mysql":
+		return StorageMysql
+	case "filesystem":
+		return StorageFilesystem
+	}
+
+	return StorageInvalid
+}
+
+/*
+
+Api generates the code for a server with the following imported games and
+given storage type, builds it, and returns the path to the compiled binary.
+
+To clean up the binary, call CleanupApi and pass the same directory.
+
+*/
+func Api(directory string, managers []string, storage StorageType) (string, error) {
+	return "", errors.New("Not yet implemented")
+}
+
+func CleanupApi(directory string) error {
+	return os.RemoveAll(directory)
+}
