8a363b77f856aa40e3ec28fba97fd9fafd1da598
Alex Komoroske <jkomoros@gmail.com>
2018-08-09 20:33:47 -0700

    Implemented build.Api. Not tested yet. Part of #655.

--- boardgame-util/lib/build/main.go
+++ boardgame-util/lib/build/main.go
@@ -13,7 +13,9 @@ import (
 	"bytes"
 	"errors"
 	"go/format"
+	"io/ioutil"
 	"os"
+	"os/exec"
 	"path/filepath"
 	"strings"
 	"text/template"
@@ -90,7 +92,7 @@ func (s StorageType) Constructor() string {
 
 }
 
-const subFolder = "api/"
+const subFolder = "api"
 
 /*
 
@@ -103,7 +105,40 @@ To clean up the binary, call CleanupApi and pass the same directory.
 
 */
 func Api(directory string, managers []string, storage StorageType) (string, error) {
-	return "", errors.New("Not yet implemented")
+
+	if _, err := os.Stat(directory); os.IsNotExist(err) {
+		return "", errors.New("The provided directory, " + directory + " does not exist.")
+	}
+
+	code, err := ApiCode(managers, storage)
+
+	if err != nil {
+		return "", errors.New("Couldn't generate code: " + err.Error())
+	}
+
+	codePath := filepath.Join(directory, subFolder, "main.go")
+
+	if err := ioutil.WriteFile(codePath, code, 0644); err != nil {
+		return "", errors.New("Couldn't save code: " + err.Error())
+	}
+
+	cmd := exec.Command("go", "build")
+	cmd.Dir = filepath.Join(directory, subFolder)
+
+	err = cmd.Run()
+
+	if err != nil {
+		return "", errors.New("Couldn't build binary: " + err.Error())
+	}
+
+	//The binary will have the name of the subfolder it was created in.
+	binaryName := filepath.Join(directory, subFolder, subFolder)
+
+	if _, err := os.Stat(binaryName); os.IsNotExist(err) {
+		return "", errors.New("Sanity check failed: binary does not appear to have been created.")
+	}
+
+	return binaryName, nil
 }
 
 //ApiCode returns the code for an api server with the given type.
