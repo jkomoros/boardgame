9feb0cd2c7ca49d0a10dbfe1c2ed98ad5d290696
Alex Komoroske <jkomoros@gmail.com>
2018-08-18 12:08:39 -0700

    Actually wired up so that `set` works correctly, but not yet for firebase or games node. Part of #655.

--- boardgame-util/cmd_config_set.go
+++ boardgame-util/cmd_config_set.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"fmt"
+	"errors"
 	"github.com/bobziuchkovski/writ"
 	"github.com/jkomoros/boardgame/boardgame-util/lib/config"
 	"strings"
@@ -14,11 +14,27 @@ type ConfigSet struct {
 	Mode   string
 }
 
+func strToBool(in string) (bool, error) {
+
+	in = strings.ToLower(in)
+
+	if in == "0" || in == "false" {
+		return false, nil
+	}
+
+	if in == "1" || in == "true" {
+		return true, nil
+	}
+
+	return false, errors.New("Invalid bool string: " + in)
+
+}
+
 func (c *ConfigSet) Run(p writ.Path, positional []string) {
 
-	//base := c.Base().(*BoardgameUtil)
+	base := c.Base().(*BoardgameUtil)
 
-	//cfg := base.GetConfig()
+	cfg := base.GetConfig()
 
 	mode := config.TypeBase
 
@@ -35,8 +51,56 @@ func (c *ConfigSet) Run(p writ.Path, positional []string) {
 		errAndQuit(c.Mode + " is not a valid mode")
 	}
 
-	fmt.Println("This command is not yet fully functional")
-	fmt.Println("Would have set", mode, c.Secret)
+	if len(positional) < 1 {
+		errAndQuit("KEY must be provided")
+	}
+
+	field := config.FieldFromString(positional[0])
+
+	fieldType := config.FieldTypes[field]
+
+	var updater config.ConfigUpdater
+
+	switch fieldType {
+	case config.FieldTypeInvalid:
+		errAndQuit(positional[0] + " is not a valid field")
+	case config.FieldTypeString:
+		if len(positional) != 2 {
+			errAndQuit("KEY of type string wants precisely one VAL")
+		}
+		updater = config.SetString(field, positional[1])
+	case config.FieldTypeStringSlice:
+		if len(positional) != 2 {
+			errAndQuit("KEY of type []string wants precisely one VAL")
+		}
+		updater = config.AddString(field, positional[1])
+	case config.FieldTypeStringMap:
+		if len(positional) != 3 {
+			errAndQuit("KEY of type map[string]string wants KEY SUB-KEY VAL")
+		}
+		updater = config.SetStringKey(field, positional[1], positional[2])
+	case config.FieldTypeBool:
+		if len(positional) != 2 {
+			errAndQuit("KEY of type bool wants one VAL")
+		}
+		b, err := strToBool(positional[1])
+		if err != nil {
+			errAndQuit(err.Error())
+		}
+		updater = config.SetBool(field, b)
+	case config.FieldTypeFirebase:
+		errAndQuit("Firebase key not yet supported")
+	case config.FieldTypeGameNode:
+		errAndQuit("GAmes not yet supported")
+	}
+
+	if err := cfg.Update(mode, c.Secret, updater); err != nil {
+		errAndQuit("Couldn't update value: " + err.Error())
+	}
+
+	if err := cfg.Save(); err != nil {
+		errAndQuit("Couldn't save updated config files: " + err.Error())
+	}
 
 }
 
@@ -67,8 +131,18 @@ func (c *ConfigSet) WritOptions() []*writ.Option {
 	}
 }
 
-//TODO: usage for positional
+func (c *ConfigSet) Usage() string {
+	return "KEY [SUB-KEY] VAL"
+}
 
 func (c *ConfigSet) HelpText() string {
-	return c.Name() + ` sets the given field to the given value in the current config.`
+	return c.Name() + ` sets the given field to the given value in the current config.
+
+If KEY is of type string, simply sets the key to the given val.
+
+If KEY is of type []string, simply adds the key to the given val if it doesn't exist.
+
+If KEY is of type bool, val must be either "0", "1", "true", "false"
+
+If KEY is of type map[key]val then SUB-KEY must also be provided.`
 }
