88601036de5b0f9c471b90f7fe7b401598b824b1
Alex Komoroske <jkomoros@gmail.com>
2018-08-18 11:22:17 -0700

    Define FirebaseKey, FirebaseKeys, FirebaseKeyFromString. Part of #655.

--- boardgame-util/lib/config/firebase.go
+++ boardgame-util/lib/config/firebase.go
@@ -1,5 +1,32 @@
 package config
 
+import (
+	"strings"
+)
+
+//FirebaseKey denotes a specific key in a firebase object
+type FirebaseKey string
+
+const (
+	FirebaseInvalid           FirebaseKey = "<INVALID>"
+	FirebaseApiKey                        = "apiKey"
+	FirebaseAuthDomain                    = "authDomain"
+	FirebaseDatabaseURL                   = "databaseURL"
+	FirebaseProjectID                     = "projectId"
+	FirebaseStorageBucket                 = "storageBucket"
+	FirebaseMessagingSenderID             = "messagingSenderId"
+)
+
+//FirebaseKeys enumerates all firebase keys
+var FirebaseKeys = map[FirebaseKey]bool{
+	FirebaseApiKey:            true,
+	FirebaseAuthDomain:        true,
+	FirebaseDatabaseURL:       true,
+	FirebaseProjectID:         true,
+	FirebaseStorageBucket:     true,
+	FirebaseMessagingSenderID: true,
+}
+
 //FirebaseConfig is a sub-struct within ConfigMode that holds values specific
 //to firebase.
 type FirebaseConfig struct {
@@ -57,4 +84,23 @@ func (f *FirebaseConfig) extend(other *FirebaseConfig) *FirebaseConfig {
 	}
 
 	return result
+}
+
+//FirebaseKeyFromString returns the FirebseKey denoted by key (fuzzy
+//matching).
+func FirebaseKeyFromString(key string) FirebaseKey {
+
+	key = strings.ToLower(key)
+	key = strings.TrimSpace(key)
+
+	for name, _ := range FirebaseKeys {
+		normalizedName := strings.ToLower(string(name))
+
+		if normalizedName == key {
+			return name
+		}
+	}
+
+	return FirebaseInvalid
+
 }
