9353792c7aad9509fb76c73228b81a2dbf9dcfd0
Alex Komoroske <jkomoros@gmail.com>
2018-08-29 06:17:54 -0700

    Made it so static.Build also creates a polymer.json based on files in game-src. Part of #655.

--- boardgame-util/lib/build/static.go
+++ boardgame-util/lib/build/static.go
@@ -18,13 +18,18 @@ const staticSubFolder = "static"
 const configJsFileName = "client_config.js"
 const gameSrcSubFolder = "game-src"
 const clientSubFolder = "client"
+const polymerConfig = "polymer.json"
+const polymerFragmentsKey = "fragments"
+const clientGameRendererFileName = "boardgame-render-game-%s.html"
+const clientPlayerInfoRendererFileName = "boardgame-render-player-info-%s.html"
 
 //The path, relative to goPath, where all of the files are to copy
 const staticServerPackage = "github.com/jkomoros/boardgame/server/static"
 
 var filesToExclude map[string]bool = map[string]bool{
-	".gitignore": true,
-	"README.md":  true,
+	".gitignore":  true,
+	"README.md":   true,
+	polymerConfig: true,
 	//Don't copy over because we'll generate our own; if we copy over and
 	//generate our own we'll overwrite original.
 	configJsFileName: true,
@@ -184,6 +189,11 @@ func Static(directory string, managers []string, c *config.Config, forceBower bo
 		return "", errors.New("Couldn't create " + gameSrcSubFolder + ": " + err.Error())
 	}
 
+	fmt.Println("Creating " + polymerConfig)
+	if err := createPolymerJson(directory, false); err != nil {
+		return "", errors.New("Couldn't create " + polymerConfig + ": " + err.Error())
+	}
+
 	return staticDir, nil
 
 }
@@ -324,3 +334,144 @@ func createConfigJs(path string, c *config.Config) error {
 func CleanStatic(directory string) error {
 	return os.RemoveAll(filepath.Join(directory, staticSubFolder))
 }
+
+//createPolymerJson outputs a polymer.json in dir/static/ that includes
+//fragments for all of the games in game-src. If missingFragmentsError is
+//true, then if there are missing fragments we will return an error.
+func createPolymerJson(dir string, missingFragmentsErrors bool) error {
+
+	path := filepath.Join(dir, staticSubFolder, polymerConfig)
+
+	if _, err := os.Stat(path); err == nil {
+		return errors.New(path + " already exists")
+	}
+
+	fragments, missingFragments, err := listClientFragments(dir)
+
+	if err != nil {
+		return errors.New("Couldn't list fragments: " + err.Error())
+	}
+
+	if len(missingFragments) > 0 {
+		if missingFragmentsErrors {
+			return errors.New("The following fragments didn't exist: " + strings.Join(missingFragments, ", "))
+		} else {
+			for _, fragment := range missingFragments {
+				fmt.Println("WARNING: missing fragment: " + fragment)
+			}
+		}
+	}
+
+	blob, err := polymerJsonContents(fragments)
+
+	if err != nil {
+		return errors.New("Couldn't create polymer config blob: " + err.Error())
+	}
+
+	return ioutil.WriteFile(path, blob, 0644)
+
+}
+
+//listClientFragments returns a list of all client fragments that should be
+//merged into polymer.config given the static dir. missingFragments is
+//fragments that really shouldn't have been there, but it's TECHNICALY legal
+//for them to not have.
+func listClientFragments(dir string) (fragments []string, missingFragments []string, err error) {
+
+	gameSrc := filepath.Join(dir, staticSubFolder, gameSrcSubFolder)
+
+	infos, err := ioutil.ReadDir(gameSrc)
+
+	if err != nil {
+		return nil, nil, errors.New("Couldn't list gameSrcs: " + err.Error())
+	}
+
+	for _, info := range infos {
+
+		clientDir := filepath.Join(gameSrc, info.Name())
+
+		gameRenderer := filepath.Join(clientDir, strings.Replace(clientGameRendererFileName, "%s", info.Name(), -1))
+
+		if _, err := os.Stat(gameRenderer); os.IsNotExist(err) {
+			missingFragments = append(missingFragments, gameRenderer)
+		} else {
+			fragments = append(fragments, gameRenderer)
+		}
+
+		playerInfoRenderer := filepath.Join(clientDir, strings.Replace(clientPlayerInfoRendererFileName, "%s", info.Name(), -1))
+
+		if _, err := os.Stat(playerInfoRenderer); os.IsNotExist(err) {
+			//playerInfoRenderer is optional
+			continue
+		}
+
+		fragments = append(fragments, playerInfoRenderer)
+
+	}
+
+	for i, fragment := range fragments {
+		fragments[i] = strings.TrimPrefix(fragment, filepath.Join(dir, staticSubFolder)+string(filepath.Separator))
+	}
+
+	return fragments, missingFragments, nil
+
+}
+
+//polymerJsonContents returns the blob representing the contents for Polymer
+//json with the given fragments.
+func polymerJsonContents(fragments []string) ([]byte, error) {
+
+	var obj map[string]interface{}
+
+	if err := json.Unmarshal([]byte(basePolymerJson), &obj); err != nil {
+		return nil, errors.New("Couldn't unmarshal base json: " + err.Error())
+	}
+
+	baseFragments, ok := obj[polymerFragmentsKey]
+
+	if !ok {
+		return nil, errors.New("Base polymer json unexpectedly didn't have fragments")
+	}
+
+	interfaces, ok := baseFragments.([]interface{})
+
+	if !ok {
+		return nil, errors.New("Base polymer json's fragments unexpectedly wasn't list of interfaces")
+	}
+
+	var strs []string
+
+	for _, inter := range interfaces {
+		str, ok := inter.(string)
+		if !ok {
+			return nil, errors.New("One of the filename strings was unexpectedly not a string")
+		}
+		strs = append(strs, str)
+	}
+
+	strs = append(strs, fragments...)
+
+	obj[polymerFragmentsKey] = strs
+
+	return json.MarshalIndent(obj, "", "\t")
+
+}
+
+const basePolymerJson = `{
+  "entrypoint": "index.html",
+  "shell": "src/boardgame-app.html",
+  "fragments": [
+    "src/boardgame-game-view.html",
+    "src/boardgame-list-games-view.html",
+    "src/boardgame-404-view.html"
+  ],
+  "sourceGlobs": [
+    "src/**/*",
+    "images/**/*",
+    "bower.json"
+  ],
+  "includeDependencies": [
+    "manifest.json",
+    "bower_components/webcomponentsjs/webcomponents-lite.min.js"
+  ]
+}`
