a2226ba97328150f66ec54fb3183ad0e903240ee
Alex Komoroske <jkomoros@gmail.com>
2018-09-02 08:01:14 -0700

    Made it so the test loads up the goldens to compare against. Part of #663.

--- boardgame-util/lib/stub/main_test.go
+++ boardgame-util/lib/stub/main_test.go
@@ -1,13 +1,19 @@
 package stub
 
 import (
+	"errors"
 	"github.com/workfit/tester/assert"
+	"io/ioutil"
+	"os"
+	"path/filepath"
 	"testing"
 )
 
 //If true, will save out the files generated. Useful for generating new golden
 //output when output is changed.
 const generateNewGolden = false
+
+const testDir = "test"
 
 func TestGenerate(t *testing.T) {
 
@@ -37,13 +43,64 @@ func TestGenerate(t *testing.T) {
 	if generateNewGolden {
 
 		//Save out contents as new golden files to compare against
-		contents.Save("test")
-
+		contents.Save(testDir)
 		return
 	}
 
-	mainGo := contents["checkers/main.go"]
+	golden, err := fileContentsFromDir(testDir)
 
-	assert.For(t).ThatActual(mainGo).IsNotNil()
+	assert.For(t).ThatActual(err).IsNil()
+
+	assert.For(t).ThatActual(contents).Equals(golden).ThenDiffOnFail()
+
+}
+
+//fileContentsFromDir loads up filecontents from the given path so they can be
+//compared to the golden.
+func fileContentsFromDir(path string) (FileContents, error) {
+
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		return nil, errors.New(path + " doesnt' exist")
+	}
+
+	result := make(FileContents)
+
+	if err := recursiveListFilesForFileContents(path, "", result); err != nil {
+		return nil, errors.New("couldn't list files: " + err.Error())
+	}
+
+	return result, nil
+
+}
+
+//basePath is actual dir to list recursively; prefix is the prefix to affix to
+//dir contenst to put in contents.
+func recursiveListFilesForFileContents(basePath, prefix string, contents FileContents) error {
+
+	infos, err := ioutil.ReadDir(basePath)
+
+	if err != nil {
+		return errors.New("Couldn't list path: " + err.Error())
+	}
+
+	for _, info := range infos {
+		if info.IsDir() {
+			if err := recursiveListFilesForFileContents(filepath.Join(basePath, info.Name()), filepath.Join(prefix, info.Name()), contents); err != nil {
+				return err
+			}
+			continue
+		}
+		//info represents a file.
+
+		content, err := ioutil.ReadFile(filepath.Join(basePath, info.Name()))
+
+		if err != nil {
+			return errors.New("couldn't read " + filepath.Join(basePath, info.Name()) + ": " + err.Error())
+		}
+
+		contents[filepath.Join(prefix, info.Name())] = content
+	}
+
+	return nil
 
 }
