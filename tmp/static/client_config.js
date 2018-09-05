3c36fcd32064e65a5f1c493efd34d17c419c0193
Alex Komoroske <jkomoros@gmail.com>
2018-09-05 05:37:31 -0700

    checked in a `boardgame-util build static --copy-files` so modulizer has it to work with and a clean git repo. Part of #656.

---
+++ tmp/static/client_config.js
@@ -0,0 +1,13 @@
+var CONFIG = {
+	"firebase": {
+		"apiKey": "AIzaSyDi0hhBgLPbpJgprVCDzDkk8zuFpb9XadM",
+		"authDomain": "example-boardgame.firebaseapp.com",
+		"databaseURL": "https://example-boardgame.firebaseio.com",
+		"projectId": "example-boardgame",
+		"storageBucket": "example-boardgame.appspot.com",
+		"messagingSenderId": "138149526364"
+	},
+	"google_analytics": "UA-321674-10",
+	"host": "https://example-boardgame.appspot.com:8080",
+	"dev_host": "http://localhost:8888"
+}
