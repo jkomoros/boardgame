530decfcc854e4a19986608a30ded1f3a78c7b2c
Alex Komoroske <jkomoros@gmail.com>
2018-09-07 18:22:48 -0700

    BROKEN Commit. Added polymer classdoc so it's OK with the mix-in function and doesn't warn. Part of #656.

--- server/static/src/boardgame-game-path.js
+++ server/static/src/boardgame-game-path.js
@@ -1,3 +1,8 @@
+/*
+ * Mix in to add GamePath. This documentation is required for the Polymer tool to not barf.
+ * @polymer
+ * @mixinFunction
+ */
 GamePathMixin = function(superClass) {
 		return class extends superClass {
 		    GamePath(name, id) {
