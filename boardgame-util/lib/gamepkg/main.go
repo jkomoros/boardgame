374e4bf8fbf730271cab9ff0f2ce60dcbdee725e
Alex Komoroske <jkomoros@gmail.com>
2018-09-18 06:02:27 -0700

    Constructors now also verify that the package is valid go package. Part of #694.

--- boardgame-util/lib/gamepkg/main.go
+++ boardgame-util/lib/gamepkg/main.go
@@ -9,7 +9,10 @@ package gamepkg
 import (
 	"errors"
 	"github.com/jkomoros/boardgame/boardgame-util/lib/path"
+	"go/ast"
 	"go/build"
+	"go/parser"
+	"go/token"
 	"io/ioutil"
 	"os"
 	"path/filepath"
@@ -18,8 +21,11 @@ import (
 type GamePkg struct {
 	//Every contstructo sets absolutePath to something that at least exists on
 	//disk.
-	absolutePath string
-	importPath   string
+	absolutePath         string
+	importPath           string
+	calculatedIsGamePkg  bool
+	memoizedIsGamePkg    bool
+	memoizedIsGamePkgErr error
 }
 
 //NewFromPath takes path (either relative or absolute path) and returns a new
@@ -75,6 +81,12 @@ func newGamePkg(absPath, importPath string) (*GamePkg, error) {
 		return nil, errors.New(absPath + " denotes a folder with no go source files")
 	}
 
+	isGamePkg, err := result.isGamePkg()
+
+	if !isGamePkg {
+		return nil, errors.New(absPath + " was not a valid game package: " + err.Error())
+	}
+
 	return result, nil
 }
 
@@ -117,4 +129,81 @@ func (g *GamePkg) Import() (string, error) {
 	}
 
 	return g.importPath, nil
+}
+
+//isGamePkg verifies that the package appears to be a valid game package.
+//Specifically it checks for
+func (g *GamePkg) isGamePkg() (bool, error) {
+	if !g.calculatedIsGamePkg {
+		g.memoizedIsGamePkg, g.memoizedIsGamePkgErr = g.calculateIsGamePkg()
+	}
+	return g.memoizedIsGamePkg, g.memoizedIsGamePkgErr
+}
+
+func (g *GamePkg) calculateIsGamePkg() (bool, error) {
+	pkgs, err := parser.ParseDir(token.NewFileSet(), g.AbsolutePath(), nil, 0)
+
+	if err != nil {
+		return false, errors.New("Couldn't parse folder: " + err.Error())
+	}
+
+	if len(pkgs) < 1 {
+		return false, errors.New("No packages in that directory")
+	}
+
+	if len(pkgs) > 1 {
+		return false, errors.New("More than one package in that directory")
+	}
+
+	var pkg *ast.Package
+
+	for _, p := range pkgs {
+		pkg = p
+	}
+
+	foundNewDelegate := false
+
+	for _, file := range pkg.Files {
+		for _, decl := range file.Decls {
+			fun, ok := decl.(*ast.FuncDecl)
+			if !ok {
+				continue
+			}
+
+			if fun.Name.String() != "NewDelegate" {
+				continue
+			}
+
+			//OK, it might be the function. Does it have the right signature?
+
+			if fun.Recv != nil {
+				return false, errors.New("NewDelegate had a receiver")
+			}
+
+			if fun.Type.Params.NumFields() > 0 {
+				return false, errors.New("NewDelegate took more than 0 items")
+			}
+
+			if fun.Type.Results.NumFields() != 1 {
+				return false, errors.New("NewDelegate didn't return exactly one item")
+			}
+
+			//TODO: check that the returned item implements
+			//boardgame.GameDelegate.
+
+			foundNewDelegate = true
+			break
+
+		}
+
+		if foundNewDelegate {
+			break
+		}
+	}
+
+	if !foundNewDelegate {
+		return false, errors.New("Couldn't find NewDelegate")
+	}
+
+	return true, nil
 }
