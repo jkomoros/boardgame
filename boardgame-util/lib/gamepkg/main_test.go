345c728ed0cee86da70919902808724be163c694
Alex Komoroske <jkomoros@gmail.com>
2018-09-18 06:09:50 -0700

    Add simple set of tests for gamepkg. Part of #694.

---
+++ boardgame-util/lib/gamepkg/main_test.go
@@ -0,0 +1,53 @@
+package gamepkg
+
+import (
+	"github.com/workfit/tester/assert"
+	"testing"
+)
+
+func TestBasic(t *testing.T) {
+	tests := []struct {
+		description string
+		importPath  string
+		errExpected bool
+	}{
+		{
+			"Basic legal",
+			"github.com/jkomoros/boardgame/examples/blackjack",
+			false,
+		},
+		{
+			"Basic illegal import",
+			"github.com/jkomoros/boardgame/NONEXISTENTFOLDER/blackjack",
+			true,
+		},
+		{
+			"No go files",
+			"github.com/jkomoros/boardgame/boardgame-util/lib",
+			true,
+		},
+		{
+			"Package doesn't have NewDelegate",
+			"github.com/jkomoros/boardgame/boardgame-util/lib/gamepkg",
+			true,
+		},
+	}
+
+	for i, test := range tests {
+		pkg, err := NewFromImport(test.importPath)
+		if test.errExpected {
+			if err == nil {
+				assert.For(t, i, test.description).ThatActual(err).IsNotNil()
+			}
+			continue
+		}
+		assert.For(t, i, test.description).ThatActual(err).IsNil()
+		assert.For(t, i, test.description).ThatActual(pkg).IsNotNil()
+
+		calculatedImport, err := pkg.Import()
+
+		assert.For(t, i, test.description).ThatActual(err).IsNil()
+
+		assert.For(t, i, test.description).ThatActual(calculatedImport).Equals(test.importPath)
+	}
+}
