4ede49977713200147ef3f58d39ff9f82456c457
Alex Komoroske <jkomoros@gmail.com>
2018-10-07 16:45:41 -0700

    Checked in a number of example games for memory now that #699 is fixed. Part of #648.

---
+++ examples/memory/golden_test.go
@@ -0,0 +1,22 @@
+package memory
+
+/*
+
+This file was created automatically by the filesystem storage layer with a
+golden folder.
+
+It will be overwritten the next time a filesystem is booted that uses this
+game package.
+
+*/
+
+import (
+	"github.com/jkomoros/boardgame/boardgame-util/lib/golden"
+	"testing"
+)
+
+func TestGolden(t *testing.T) {
+	if err := golden.CompareFolder(NewDelegate(), "testdata/golden"); err != nil {
+		t.Error(err.Error())
+	}
+}
