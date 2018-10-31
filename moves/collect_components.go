jkomoros/boardgame

commit 76af12a692012363690259eb7a1f4ababbb97c32

All of the Move,Deal,Count* moves now check that the source stack has at least one component to move, and that the destination stack has at least one slot, in its Legal method. Noticed while looking at #458.

moves/deal_components.go
@@ -223,6 +223,45 @@ func (d *DealCountComponents) ValidConfiguration(exampleState boardgame.State) e
 	return d.RoundRobinNumRounds.ValidConfiguration(exampleState)
 }
 
+func (d *DealCountComponents) Legal(state boardgame.ImmutableState, proposer boardgame.PlayerIndex) error {
+	if err := d.RoundRobinNumRounds.Legal(state, proposer); err != nil {
+		return err
+	}
+
+	nextPlayerIndex, _ := d.nextPlayerIndex(state)
+	immutablePlayerState := state.ImmutablePlayerStates()[nextPlayerIndex]
+
+	//Yes. this is a hack. :-/
+	playerState, ok := immutablePlayerState.(boardgame.PlayerState)
+	if !ok {
+		return errors.New("ImmutablePlayerState couldn't be casted to PlayerState")
+	}
+
+	destination, source, err := dealActionHelper(d.TopLevelStruct(), playerState)
+
+	if err != nil {
+		return err
+	}
+
+	if source == nil {
+		return errors.New("Source was nil")
+	}
+
+	if destination == nil {
+		return errors.New("Destination was nil")
+	}
+
+	if source.NumComponents() < 1 {
+		return errors.New("The stack to draw from doesn't have any components to move!")
+	}
+
+	if destination.SlotsRemaining() < 1 {
+		return errors.New("The destination stack doesn't have any slots to move the component to!")
+	}
+
+	return nil
+}
+
 //RoundRobinAction moves a component from the GameStack to the PlayerStack, as
 //configured by the PlayerStacker and GameStacker interfaces.
 func (d *DealCountComponents) RoundRobinAction(playerState boardgame.PlayerState) error {

moves/move_components.go
@@ -134,6 +134,33 @@ func (m *MoveCountComponents) stackNames(state boardgame.ImmutableState) (starte
 	return stackName(m, configPropSourceProperty, sourceStack, state), stackName(m, configPropDestinationProperty, destinationStack, state)
 }
 
+func (m *MoveCountComponents) Legal(state boardgame.ImmutableState, proposer boardgame.PlayerIndex) error {
+	if err := m.ApplyCountTimes.Legal(state, proposer); err != nil {
+		return err
+	}
+
+	source, destination := m.stacks(state)
+
+	if source == nil {
+		return errors.New("Source was nil")
+	}
+
+	if destination == nil {
+		return errors.New("Destination was nil")
+	}
+
+	if source.NumComponents() < 1 {
+		return errors.New("The stack to draw from doesn't have any components to move!")
+	}
+
+	if destination.SlotsRemaining() < 1 {
+		return errors.New("The destination stack doesn't have any slots to move the component to!")
+	}
+
+	return nil
+
+}
+
 //Apply by default moves one component from SourceStack() to
 //DestinationStack(). You likely do not need to override this method.
 func (m *MoveCountComponents) Apply(state boardgame.State) error {

moves/round_robin.go
@@ -178,11 +178,11 @@ func (r *RoundRobin) finishRoundRobin(state boardgame.State) error {
 //operate on.  Also returns roundSkip true if the player we end on is the
 //first player in the round robin, or if we skipped over them to find the next
 //valid player.
-func (r *RoundRobin) nextPlayerIndex(state boardgame.State) (player boardgame.PlayerIndex, roundSkip bool) {
+func (r *RoundRobin) nextPlayerIndex(state boardgame.ImmutableState) (player boardgame.PlayerIndex, roundSkip bool) {
 
 	var currentPlayer boardgame.PlayerIndex
 
-	roundRobiner, ok := state.GameState().(interfaces.RoundRobinProperties)
+	roundRobiner, ok := state.ImmutableGameState().(interfaces.RoundRobinProperties)
 
 	if !ok {
 		return boardgame.ObserverPlayerIndex, true
@@ -222,7 +222,7 @@ func (r *RoundRobin) nextPlayerIndex(state boardgame.State) (player boardgame.Pl
 	roundSkip = false
 
 	//Advance around, but if we loop back just leave it.
-	for counter <= len(state.PlayerStates()) {
+	for counter <= len(state.ImmutablePlayerStates()) {
 
 		currentPlayer = currentPlayer.Next(state)
 
@@ -230,14 +230,14 @@ func (r *RoundRobin) nextPlayerIndex(state boardgame.State) (player boardgame.Pl
 			roundSkip = true
 		}
 
-		if !conditionsMet.PlayerConditionMet(state.PlayerStates()[currentPlayer]) {
+		if !conditionsMet.PlayerConditionMet(state.ImmutablePlayerStates()[currentPlayer]) {
 			break
 		}
 
 		counter++
 	}
 
-	if counter > len(state.PlayerStates()) {
+	if counter > len(state.ImmutablePlayerStates()) {
 		//No players are legal
 		return currentPlayer, true
 	}

commit ecdca2c51e5395069b4502153720335374b2fb0a

Fixed bug introduced in 76af12a where Collect() components could report that there wasn't any room in the destination erroneously for all Collect* moves. Part of #727. Part of #458.

moves/collect_components.go
@@ -19,6 +19,10 @@ type CollectCountComponents struct {
 	DealCountComponents
 }
 
+func (d *CollectCountComponents) sourceAndDestination(playerStack boardgame.Stack, gameStack boardgame.Stack) (source boardgame.Stack, destination boardgame.Stack) {
+	return playerStack, gameStack
+}
+
 //RoundRobinAction moves a component from the PlayerStack to the GameStack, as
 //configured by the PlayerStacker and GameStacker interfaces.
 func (d *CollectCountComponents) RoundRobinAction(playerState boardgame.PlayerState) error {
@@ -64,6 +68,10 @@ func (d *CollectCountComponents) FallbackHelpText() string {
 //boardgame:codegen
 type CollectComponentsUntilPlayerCountLeft struct {
 	DealComponentsUntilPlayerCountReached
+}
+
+func (d *CollectComponentsUntilPlayerCountLeft) sourceAndDestination(playerStack boardgame.Stack, gameStack boardgame.Stack) (source boardgame.Stack, destination boardgame.Stack) {
+	return playerStack, gameStack
 }
 
 //RoundRobinAction moves a component from the PlayerStack to the GameStack, as
@@ -123,6 +131,10 @@ func (d *CollectComponentsUntilPlayerCountLeft) FallbackHelpText() string {
 //boardgame:codegen
 type CollectComponentsUntilGameCountReached struct {
 	DealComponentsUntilGameCountLeft
+}
+
+func (d *CollectComponentsUntilGameCountReached) sourceAndDestination(playerStack boardgame.Stack, gameStack boardgame.Stack) (source boardgame.Stack, destination boardgame.Stack) {
+	return playerStack, gameStack
 }
 
 //RoundRobinAction moves a component from the PlayerStack to the GameStack, as

moves/deal_components.go
@@ -223,6 +223,17 @@ func (d *DealCountComponents) ValidConfiguration(exampleState boardgame.State) e
 	return d.RoundRobinNumRounds.ValidConfiguration(exampleState)
 }
 
+type sourceAndDestinationer interface {
+	sourceAndDestination(playerStack boardgame.Stack, gameStack boardgame.Stack) (source boardgame.Stack, destination boardgame.Stack)
+}
+
+//sourceAndDestination takes the playerStack and gameStrack and returns the
+//source and desintation, for this move. All Deal* moves should return
+//gameStack, PlayerStack; all Collect* moves should return the opposite.
+func (d *DealCountComponents) sourceAndDestination(playerStack boardgame.Stack, gameStack boardgame.Stack) (source boardgame.Stack, destination boardgame.Stack) {
+	return gameStack, playerStack
+}
+
 func (d *DealCountComponents) Legal(state boardgame.ImmutableState, proposer boardgame.PlayerIndex) error {
 	if err := d.RoundRobinNumRounds.Legal(state, proposer); err != nil {
 		return err
@@ -237,7 +248,15 @@ func (d *DealCountComponents) Legal(state boardgame.ImmutableState, proposer boa
 		return errors.New("ImmutablePlayerState couldn't be casted to PlayerState")
 	}
 
-	destination, source, err := dealActionHelper(d.TopLevelStruct(), playerState)
+	playerStack, gameStack, err := dealActionHelper(d.TopLevelStruct(), playerState)
+
+	sAndD, ok := d.TopLevelStruct().(sourceAndDestinationer)
+
+	if !ok {
+		return errors.New("Source and Destination overridden")
+	}
+
+	source, destination := sAndD.sourceAndDestination(playerStack, gameStack)
 
 	if err != nil {
 		return err