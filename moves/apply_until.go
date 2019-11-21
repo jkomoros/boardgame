jkomoros/boardgame

commit 93b44a1e5a78192a3c6197ba7338ea7be829acfb

Add StateGroupComponentValues so that StatePropertyRef can also fetch ComponentValues. Operates almost exactly the same as StateGroupDynamicComponentValues. Part of #737. Tested.
Note: there's a nil panic in this commit if you pass an exampleState that was generated from GameManager.ExampleState. That will be fixed in the next commit.

state.go
@@ -118,6 +118,9 @@ const (
 	StateGroupGame StateGroupType = iota
 	//StateGroupPlayer refers to the PlayerState part of State
 	StateGroupPlayer
+	//StateGroupComponentValues referes to the non-dynamic values of the given
+	//component.
+	StateGroupComponentValues
 	//StateGroupDynamicComponentValues refers to the DynamicComponentValues part
 	//of State.
 	StateGroupDynamicComponentValues
@@ -134,16 +137,18 @@ type StatePropertyRef struct {
 	//PropName is the specific property on the given SubStateObject specified
 	//by the rest of the StatePropertyRef.
 	PropName string
-	//DeckName is only used when Group is StateGroupDynamicComponentValues
+	//DeckName is only used when Group is StateGroupComponentValues or
+	//StateGroupDynamicComponentValues
 	DeckName string
 
 	//PlayerIndex is the index of the player, if Group is StateGroupPlayer and
 	//the intent of the StatePropertyRef is to select a specific player's state.
 	//0 is always legal.
 	PlayerIndex int
-	//DeckIndex is used only when the Group is StateGroupDynamicComponentValues
-	//and the intent of the StatePropertyRef is to select a specific
-	//DynamicComponentValues. 0 is always legal.
+	//DeckIndex is used only when the Group is StateGroupComponentValues or
+	//StateGroupDynamicComponentValues and the intent of the StatePropertyRef is
+	//to select a specific ComponentValues or DynamicComponentValues. 0 is
+	//always legal.
 	DeckIndex int
 }
 
@@ -219,6 +224,26 @@ func (r StatePropertyRef) Reader(state ImmutableState) (PropertyReader, error) {
 			return nil, errors.New("PlayerState was nil")
 		}
 		reader = st.Reader()
+	case StateGroupComponentValues:
+		deck := state.Game().Manager().Chest().Deck(r.DeckName)
+		if deck == nil {
+			return nil, errors.New("That deck name is not valid " + r.DeckName)
+		}
+		if deck.Len() == 0 {
+			return nil, errors.New("No components for deck " + r.DeckName)
+		}
+		if r.DeckIndex < 0 {
+			return nil, errors.New("Invalid low DeckIndex")
+		}
+		if r.DeckIndex >= deck.Len() {
+			return nil, errors.New("DeckIndex too high")
+		}
+		component := deck.ComponentAt(r.DeckIndex)
+		st := component.Values()
+		if st == nil {
+			return nil, errors.New("No Values in Component")
+		}
+		reader = st.Reader()
 	case StateGroupDynamicComponentValues:
 		states := state.ImmutableDynamicComponentValues()[r.DeckName]
 		if len(states) == 0 {
@@ -246,7 +271,7 @@ func (r StatePropertyRef) Reader(state ImmutableState) (PropertyReader, error) {
 //property exists, and if Index properties are non-default, that they denote a
 //valid index.
 func (r StatePropertyRef) Validate(exampleState ImmutableState) error {
-	if r.Group != StateGroupGame && r.Group != StateGroupPlayer && r.Group != StateGroupDynamicComponentValues {
+	if r.Group != StateGroupGame && r.Group != StateGroupPlayer && r.Group != StateGroupComponentValues && r.Group != StateGroupDynamicComponentValues {
 		return errors.New("group is set to an invalid value, must be one of Game, Player, DynamicComponentValues")
 	}
 
@@ -271,13 +296,19 @@ func (r StatePropertyRef) Validate(exampleState ImmutableState) error {
 	}
 
 	//Check DeckName is valid
-	if r.Group == StateGroupDynamicComponentValues {
+	if r.Group == StateGroupComponentValues || r.Group == StateGroupDynamicComponentValues {
 		if r.DeckName == "" {
-			return errors.New("No DeckName provided for GroupDynamicComponentValues, but it's required")
+			return errors.New("No DeckName provided for GroupComponentValues, but it's required")
 		}
 		if exampleState != nil {
-			if _, ok := exampleState.ImmutableDynamicComponentValues()[r.DeckName]; !ok {
-				return errors.New("DeckName selected a deck that doesn't exist")
+			if r.Group == StateGroupComponentValues {
+				if deck := exampleState.Game().Manager().Chest().Deck(r.DeckName); deck == nil {
+					return errors.New("DeckName selected a deck that doesn't exist")
+				}
+			} else {
+				if _, ok := exampleState.ImmutableDynamicComponentValues()[r.DeckName]; !ok {
+					return errors.New("DeckName selected a deck that doesn't exist")
+				}
 			}
 		}
 	} else {

game_delegate.go
@@ -231,9 +231,10 @@ type GameDelegate interface {
 	//mamber of. In practice the default behavior of base.GameDelegate, which
 	//uses struct tags to figure out the policy, is sufficient and you do not
 	//need to override this. For more on how sanitization works, see the
-	//documenation for Policy. The statePropetyRef passed will always have the
-	//Index properties set to 0, but remember that the returned Policy will be
-	//applied to all Indexes.
+	//documenation for Policy. The statePropetyRef passed will never be
+	//StateGroupComponentValues, and will always have the Index properties set
+	//to 0, but remember that the returned Policy will be applied to all
+	//Indexes.
 	SanitizationPolicy(prop StatePropertyRef, groupMembership map[int]bool) Policy
 
 	//If you have computed properties that you want to be included in your

state_test.go
@@ -52,6 +52,15 @@ func TestStatePropertyRefValidate(t *testing.T) {
 			false,
 		},
 		{
+			"Basic existing component values property",
+			StatePropertyRef{
+				Group:    StateGroupComponentValues,
+				PropName: "Integer",
+				DeckName: "test",
+			},
+			false,
+		},
+		{
 			"Basic existing game property that doesn't exist",
 			StatePropertyRef{
 				Group:    StateGroupGame,
@@ -77,10 +86,27 @@ func TestStatePropertyRefValidate(t *testing.T) {
 			true,
 		},
 		{
+			"Basic existing component values property that doesn't exist",
+			StatePropertyRef{
+				Group:    StateGroupComponentValues,
+				PropName: "Nonexistent",
+				DeckName: "test",
+			},
+			true,
+		},
+		{
 			"Basic existing dynamic component values property missing deck",
 			StatePropertyRef{
 				Group:    StateGroupDynamicComponentValues,
 				PropName: "IntVar",
+			},
+			true,
+		},
+		{
+			"Basic existing component values property missing deck",
+			StatePropertyRef{
+				Group:    StateGroupComponentValues,
+				PropName: "Integer",
 			},
 			true,
 		},
@@ -94,10 +120,29 @@ func TestStatePropertyRefValidate(t *testing.T) {
 			true,
 		},
 		{
+			"Basic existing component values property invalid deck",
+			StatePropertyRef{
+				Group:    StateGroupComponentValues,
+				PropName: "Integer",
+				DeckName: "invaliddeckname",
+			},
+			true,
+		},
+		{
 			"Basic existing dynamic component values property invalid index",
 			StatePropertyRef{
 				Group:     StateGroupDynamicComponentValues,
 				PropName:  "IntVar",
+				DeckName:  "test",
+				DeckIndex: 1000,
+			},
+			true,
+		},
+		{
+			"Basic existing component values property invalid index",
+			StatePropertyRef{
+				Group:     StateGroupComponentValues,
+				PropName:  "Integer",
 				DeckName:  "test",
 				DeckIndex: 1000,
 			},

commit 947529b8c4fa4b69c4e752e5bd43c6feac0d08d8

Remove all use of state.Game().Manager() and replace with state.Manager(), which is safer. Fixes panic in edge cases that was introduced in 93b44a1e5a78192a3c6197ba7338ea7be829acfb. Part of #737.

moves/apply_until.go
@@ -197,7 +197,7 @@ func countMovesApplied(topLevelStruct boardgame.Move, state boardgame.ImmutableS
 	}
 
 	targetName := topLevelStruct.Info().Name()
-	targetPhase := state.Game().Manager().Delegate().CurrentPhase(state)
+	targetPhase := state.Manager().Delegate().CurrentPhase(state)
 
 	count := 0
 

moves/default.go
@@ -111,7 +111,7 @@ func (d *Default) ValidConfiguration(exampleState boardgame.State) error {
 			return errors.New("Legal Phases unexpectedly were not ints")
 		}
 
-		delegate := exampleState.Game().Manager().Delegate()
+		delegate := exampleState.Manager().Delegate()
 
 		phaseEnum := delegate.PhaseEnum()
 
@@ -364,7 +364,7 @@ func (d *Default) legalInPhase(state boardgame.ImmutableState) error {
 		return nil
 	}
 
-	delegate := state.Game().Manager().Delegate()
+	delegate := state.Manager().Delegate()
 
 	phaseEnum := delegate.PhaseEnum()
 
@@ -501,7 +501,7 @@ func (d *Default) legalMoveInProgression(state boardgame.ImmutableState, propose
 		return nil
 	}
 
-	currentPhase := state.Game().Manager().Delegate().CurrentPhase(state)
+	currentPhase := state.Manager().Delegate().CurrentPhase(state)
 
 	historicalMoves := d.historicalMovesSincePhaseTransition(state.Game(), state.Version(), currentPhase)
 

moves/round_robin.go
@@ -87,7 +87,7 @@ type RoundRobin struct {
 //RoundRobinStarterPlayer by default will return delegate.CurrentPlayer.
 //Override this method if you want a different starter.
 func (r *RoundRobin) RoundRobinStarterPlayer(state boardgame.ImmutableState) boardgame.PlayerIndex {
-	return state.Game().Manager().Delegate().CurrentPlayerIndex(state)
+	return state.Manager().Delegate().CurrentPlayerIndex(state)
 }
 
 //ConditionMet  goes around and returns nil if all players have had their

moves/start_phase.go
@@ -46,7 +46,7 @@ func (s *StartPhase) ValidConfiguration(exampleState boardgame.State) error {
 		return errors.New("The embedding move does not have PhaseToStart()")
 	}
 
-	delegate := exampleState.Game().Manager().Delegate()
+	delegate := exampleState.Manager().Delegate()
 
 	phaseToStart := phaseStarter.PhaseToStart(delegate.CurrentPhase(exampleState))
 
@@ -104,7 +104,7 @@ func (s *StartPhase) Apply(state boardgame.State) error {
 		return errors.New("The embedding move does not have PhaseToStart()")
 	}
 
-	currentPhase := state.Game().Manager().Delegate().CurrentPhase(state)
+	currentPhase := state.Manager().Delegate().CurrentPhase(state)
 
 	phaseToEnter := phaseEnterer.PhaseToStart(currentPhase)
 

state.go
@@ -236,7 +236,7 @@ func (r StatePropertyRef) Reader(state ImmutableState) (PropertyReader, error) {
 		}
 		reader = st.Reader()
 	case StateGroupComponentValues:
-		deck := state.Game().Manager().Chest().Deck(r.DeckName)
+		deck := state.Manager().Chest().Deck(r.DeckName)
 		if deck == nil {
 			return nil, errors.New("That deck name is not valid " + r.DeckName)
 		}
@@ -313,7 +313,7 @@ func (r StatePropertyRef) Validate(exampleState ImmutableState) error {
 		}
 		if exampleState != nil {
 			if r.Group == StateGroupComponentValues {
-				if deck := exampleState.Game().Manager().Chest().Deck(r.DeckName); deck == nil {
+				if deck := exampleState.Manager().Chest().Deck(r.DeckName); deck == nil {
 					return errors.New("DeckName selected a deck that doesn't exist")
 				}
 			} else {
@@ -870,7 +870,7 @@ func (s *state) validateBeforeSave() error {
 
 	//If delegate.PhaseEnum returns a tree, ensure it's in a leaf state.
 
-	delegate := s.Game().Manager().Delegate()
+	delegate := s.Manager().Delegate()
 
 	e := delegate.PhaseEnum()
 