c6e8a8aeb93d1f565081758efb40a63bd4b85cd1
Alex Komoroske <jkomoros@gmail.com>
2019-11-22 11:05:16 -0800

    Add tests that verify that behaviors.RoundRobin and CurrentPlayer implement the moves/interfaces they intend to implement, which will help detect drift if those interfaces change.
    Part of #741.

--- behaviors/main_test.go
+++ behaviors/main_test.go
@@ -3,6 +3,8 @@ package behaviors
 import (
 	"testing"
 
+	"github.com/jkomoros/boardgame/moves/interfaces"
+
 	"github.com/workfit/tester/assert"
 )
 
@@ -11,11 +13,15 @@ func TestRoundRobin(t *testing.T) {
 	b = &RoundRobin{}
 	_, ok := b.(Interface)
 	assert.For(t).ThatActual(ok).IsTrue()
+	_, ok = b.(interfaces.RoundRobinProperties)
+	assert.For(t).ThatActual(ok).IsTrue()
 }
 
 func TestCurrentPlayer(t *testing.T) {
 	var b interface{}
 	b = &CurrentPlayer{}
 	_, ok := b.(Interface)
+	assert.For(t).ThatActual(ok).IsTrue()
+	_, ok = b.(interfaces.CurrentPlayerSetter)
 	assert.For(t).ThatActual(ok).IsTrue()
 }
