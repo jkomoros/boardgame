jkomoros/boardgame

commit 4dec87dd04ae89e31524304746d11ebc2dfa1a3f

filesystem records now are created in 'relative move mode' where it stores the Version and Initiator as relative values, not absolute values.
The filesystem machinery will read old absolute-style move records fine, but will save new ones in relative mode.

Relative mode is more convenient for goldens, because when you need to inject a few moves, you no longer have to modify the Version and Initiator fields for all subsequent records.

Part of #755. Part of #764.

storage/filesystem/record/main.go
@@ -57,7 +57,14 @@ type Record struct {
 }
 
 type storageRecord struct {
-	Game  *boardgame.GameStorageRecord
+	Game *boardgame.GameStorageRecord
+	//Moves can be in 'absolute' mode, where their Version and Initiator fields
+	//are positive values, or 'relative mode, where their Version is -1. In
+	//relative mode, when Record.Move() is called, the MoveStorageRecord will
+	//have the version automatically set to the right version number, and its
+	//Initiator version will be added to its version number to give the right
+	//absolute number. This library handles moves in either mode on disk, but
+	//new moves added will be stored in relative mode.
 	Moves []*boardgame.MoveStorageRecord
 	//StatePatches are diffs from the state before. Get the actual state for a
 	//version with State().
@@ -318,7 +325,23 @@ func (r *Record) Move(version int) (*boardgame.MoveStorageRecord, error) {
 		return nil, errors.New("Not enough moves")
 	}
 
-	return r.data.Moves[version], nil
+	rec := r.data.Moves[version]
+
+	if rec.Version >= 0 {
+		//It's in absolute mode already
+		return rec, nil
+	}
+
+	//It's in relative mode, return a copy that's de-relativized
+
+	recCopy := new(boardgame.MoveStorageRecord)
+	*recCopy = *rec
+
+	//We removed one from version to fetch it
+	recCopy.Version = version + 1
+	recCopy.Initiator = recCopy.Version + recCopy.Initiator
+
+	return recCopy, nil
 }
 
 //randomString returns a random string of the given length.
@@ -448,6 +471,11 @@ func (r *Record) AddGameAndCurrentState(game *boardgame.GameStorageRecord, state
 	r.data.Game = game
 
 	if move != nil {
+		moveCopy := new(boardgame.MoveStorageRecord)
+		*moveCopy = *move
+		//Store in relative mode (see comments on storageRecord.Moves)
+		moveCopy.Initiator = -1 * (moveCopy.Version - moveCopy.Initiator)
+		moveCopy.Version = -1
 		r.data.Moves = append(r.data.Moves, move)
 	}
 

storage/filesystem/record/main_test.go
@@ -8,6 +8,7 @@ import (
 
 const fullJSONFilename = "testdata/full.json"
 const diffJSONFilename = "testdata/diff.json"
+const relativeDiffJSONFilename = "testdata/relative_diff.json"
 
 func TestEncoding(t *testing.T) {
 
@@ -78,6 +79,26 @@ func TestExpand(t *testing.T) {
 	err = canonicalRec.compare(rec)
 	assert.For(t).ThatActual(err).IsNil()
 
+}
+
+func TestRelativeMoves(t *testing.T) {
+	canonicalRec, err := New(diffJSONFilename)
+
+	assert.For(t).ThatActual(err).IsNil()
+
+	rec, err := New(relativeDiffJSONFilename)
+
+	assert.For(t).ThatActual(err).IsNil()
+
+	assert.For(t).ThatActual(len(canonicalRec.data.Moves)).Equals(len(rec.data.Moves))
+
+	for i := 1; i < len(rec.data.Moves); i++ {
+		canonicalMove, err := canonicalRec.Move(i)
+		assert.For(t, i).ThatActual(err).IsNil()
+		move, err := rec.Move(i)
+		assert.For(t, i).ThatActual(err).IsNil()
+		assert.For(t, i).ThatActual(move).Equals(canonicalMove).ThenDiffOnFail()
+	}
 }
 
 //TODO: actually test the behavior where we flip from compressed to expanded

commit af273e3d7d181179cb93e95ca6b4b2e69ff19575

Fixed a bug introduced in 4dec87dd04ae89e31524304746d11ebc2dfa1a3f where we actually didn't save the relativized version. Part of #755. Part of #764. Part of #701.

storage/filesystem/record/main.go
@@ -504,7 +504,7 @@ func (r *Record) AddGameAndCurrentState(game *boardgame.GameStorageRecord, state
 		//Store in relative mode (see comments on storageRecord.Moves)
 		moveCopy.Initiator = -1 * (moveCopy.Version - moveCopy.Initiator)
 		moveCopy.Version = -1
-		r.data.Moves = append(r.data.Moves, move)
+		r.data.Moves = append(r.data.Moves, moveCopy)
 	}
 
 	r.states = append(r.states, state)