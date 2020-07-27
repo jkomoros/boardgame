b39b01b2bc453be292214539f5ba87e5aa038685
Alex Komoroske <jkomoros@gmail.com>
2020-07-27 16:43:01 -0700

    Golden's own tests use the golden_test idiom of accepting -update-golden. Part of #701. Part of #755.

--- boardgame-util/lib/golden/main_test.go
+++ boardgame-util/lib/golden/main_test.go
@@ -1,22 +1,24 @@
 package golden
 
 import (
+	"flag"
 	"testing"
 
 	"github.com/jkomoros/boardgame/examples/blackjack"
 	"github.com/workfit/tester/assert"
 )
 
+var updateGolden = flag.Bool("update-golden", false, "update golden files if they're different instead of erroring")
+
 func TestBasic(t *testing.T) {
 
-	err := Compare(blackjack.NewDelegate(), "test/basic_blackjack.json", false)
+	err := Compare(blackjack.NewDelegate(), "test/basic_blackjack.json", *updateGolden)
 
 	assert.For(t).ThatActual(err).IsNil()
 
 }
 
 func TestFolder(t *testing.T) {
-	err := CompareFolder(blackjack.NewDelegate(), "test", false)
-
+	err := CompareFolder(blackjack.NewDelegate(), "test", *updateGolden)
 	assert.For(t).ThatActual(err).IsNil()
 }
