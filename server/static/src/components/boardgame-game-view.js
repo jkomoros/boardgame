6a8282f1576a841c8971edf029d26aff4e5bc2c2
Alex Komoroske <jkomoros@gmail.com>
2020-08-01 16:47:28 -0700

    Big commit. boardgame-game-view switches to being a lit-element. Everyting SEEMS to work? Part of #614.

--- server/static/src/components/boardgame-game-view.js
+++ server/static/src/components/boardgame-game-view.js
@@ -1,27 +1,22 @@
-/**
-@license
-Copyright (c) 2016 The Polymer Project Authors. All rights reserved.
-This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
-The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
-The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
-Code distributed by Google as part of the polymer project is also
-subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
-*/
-import { PolymerElement } from '@polymer/polymer/polymer-element.js';
 
-import '@polymer/polymer/lib/elements/dom-if.js';
-import '@polymer/polymer/lib/elements/dom-repeat.js';
+import { LitElement, html } from '@polymer/lit-element';
 import './boardgame-player-roster.js';
 import './shared-styles.js';
 import './boardgame-render-game.js';
 import './boardgame-admin-controls.js';
 import './boardgame-game-state-manager.js';
-import { html } from '@polymer/polymer/lib/utils/html-tag.js';
+import { SharedStyles } from './shared-styles-lit.js';
+import {
+  deepCopy,
+  getProperty,
+  setProperty,
+} from '../util.js';
 
-class BoardgameGameView extends PolymerElement {
-  static get template() {
+class BoardgameGameView extends LitElement {
+  render() {
     return html`
-    <style include="shared-styles iron-flex">
+    ${ SharedStyles }
+    <style>
       :host {
         display: block;
         --animation-length: 0.5s;
@@ -49,77 +44,59 @@ class BoardgameGameView extends PolymerElement {
     </style>
 
     <div class="card">
-      <boardgame-player-roster id="player" logged-in="[[loggedIn]]" game-route="[[gameRoute]]" viewing-as-player="[[viewingAsPlayer]]" has-empty-slots="[[hasEmptySlots]]" game-open="[[gameOpen]]" game-visible="[[gameVisible]]" current-player-index="[[game.CurrentPlayerIndex]]" players-info="[[playersInfo]]" state="[[currentState]]" finished="[[game.Finished]]" winners="[[game.Winners]]" admin="[[admin]]" is-owner="[[isOwner]]" active="[[selected]]"></boardgame-player-roster>
+      <boardgame-player-roster id="player" .loggedIn=${this.loggedIn} .gameRoute=${this.gameRoute} .viewingAsPlayer=${this.viewingAsPlayer} .hasEmptySlots=${this.hasEmptySlots} .gameOpen=${this.gameOpen} .gameVisible=${this.gameVisible} .currentPlayerIndex=${this.game ? this.game.CurrentPlayerIndex : 0} .playersInfo=${this.playersInfo} .state=${this.currentState} .finished=${this.game ? this.game.Finished : false} .winners=${this.game ? this.game.Winners : []} .admin=${this.admin} isOwner=${this.isOwner} .active=${this.selected}></boardgame-player-roster>
     </div>
     <div class="card">
-      <boardgame-render-game id="render" state="[[currentState]]" diagram="[[game.Diagram]]" renderer="{{activeRenderer}}" game-name="[[gameRoute.name]]" viewing-as-player="[[viewingAsPlayer]]" current-player-index="[[game.CurrentPlayerIndex]]" socket-active="[[socketActive]]" active="[[selected]]" chest="[[chest]]"></boardgame-render-game>
+      <boardgame-render-game id="render" .state=${this.currentState} .diagram=${this.game ? this.game.Diagram : ""} .renderer=${this.activeRenderer} @renderer-changed=${this._handleRendererChanged} .gameName=${this.gameRoute ? this.gameRoute.name : ""} .viewingAsPlayer=${this.viewingAsPlayer} .currentPlayerIndex=${this.game ? this.game.CurrentPlayerIndex : 0} .socketActive=${this.socketActive} .active=${this.selected} .chest=${this.chest}></boardgame-render-game>
     </div>
-    <boardgame-admin-controls id="admin" active="{{admin}}" game="[[game]]" viewing-as-player="[[viewingAsPlayer]]" move-forms="[[moveForms]]" game-route="[[gameRoute]]" chest="[[chest]]" game-state="[[gameState]]" requested-player="{{requestedPlayer}}" auto-current-player="{{autoCurrentPlayer}}"></boardgame-admin-controls>
-    <boardgame-game-state-manager id="manager" active-renderer="[[activeRenderer]]" game-route="[[gameRoute]]" requested-player="[[requestedPlayer]]" active="[[selected]]" admin="[[admin]]" game-finished="[[game.Finished]]" game-version="[[game.Version]]" logged-in="[[loggedIn]]" auto-current-player="[[autoCurrentPlayer]]" viewing-as-player="[[viewingAsPlayer]]" socket-active="{{socketActive}}"></boardgame-game-state-manager>
+    <boardgame-admin-controls id="admin" .active=${this.admin} .game=${this.game} .viewingAsPlayer=${this.viewingAsPlayer} .moveForms=${this.moveForms} .gameRoute=${this.gameRoute} .chest=${this.chest} .gameState=${this.gameState} .requestedPlayer=${this.requestedPlayer} @requested-player-changed=${this._handleRequestedPlayerChanged} .autoCurrentPlayer=${this.autoCurrentPlayer} @auto-current-player-changed=${this._handleAutoCurrentPlayerChanged}></boardgame-admin-controls>
+    <boardgame-game-state-manager id="manager" .activeRenderer=${this.activeRenderer} .gameRoute=${this.gameRoute} .requestedPlayer=${this.requestedPlayer} .active=${this.selected} .admin=${this.admin} .gameFinished=${this.game ? this.game.Finished : false} .gameVersion=${this.game ? this.game.Version : 0} .loggedIn=${this.loggedIn} .autoCurrentPlayer=${this.autoCurrentPlayer} .viewingAsPlayer=${this.viewingAsPlayer} .socketActive=${this.socketActive} @socket-active-changed=${this._handleSocketActiveChanged}></boardgame-game-state-manager>
 `;
-  }
-
-  static get is() {
-    return "boardgame-game-view"
   }
 
   static get properties() {
     return {
-
-      requestedPlayer: {
-        type: Number,
-        value: 0,
-      },
-      gameState : String,
-      game: Object,
-      currentState: Object,
-      chest : Object,
-      playersInfo: Array,
-      hasEmptySlots: Boolean,
-      gameOpen: Boolean,
-      gameVisible: Boolean,
-      isOwner: Boolean,
-      gameRoute: {
-        type: Object,
-        observer: "_gameRouteChanged"
-      },
-      autoCurrentPlayer: Boolean,
-      admin: Boolean,
-      selected: {
-        type: Boolean,
-        observer: "_selectedChanged",
-      },
-      loggedIn: Boolean,
-      promptedToJoin: {
-        type: Boolean,
-        value: false,
-      },
-      pathsToTick: {
-        type: Array
-      },
-      originalWallClockStartTime: {
-        type: Number
-      },
-      viewingAsPlayer: {
-        type: Number,
-        value: 0,
-      },
+      requestedPlayer: { type: Number },
+      gameState: { type: String },
+      game: { type: Object },
+      currentState: { type: Object },
+      chest: { type: Object },
+      playersInfo: { type: Array },
+      hasEmptySlots: { type: Boolean },
+      gameOpen: { type: Boolean },
+      gameVisible: { type: Boolean },
+      isOwner: { type: Boolean },
+      gameRoute: { type: Object },
+      autoCurrentPlayer: { type: Boolean },
+      admin: { type: Boolean },
+      selected: { type: Boolean },
+      loggedIn: { type: Boolean },
+      promptedToJoin: { type: Boolean },
+      pathsToTick: { type: Array },
+      originalWallClockStartTime: { type: Number },
+      viewingAsPlayer: { type: Number },
       //The current renderer, passed up from the gameRenderer, so we can pass
       //it to stateGameManager and readyForNextState.
-      activeRenderer : Object,
-      moveForms: Object,
-      socketActive: Boolean,
-      _firstStateBundle: {
-        type: Boolean,
-        value: true,
-      }
+      activeRenderer : { type: Object },
+      moveForms: { type: Object },
+      socketActive: { type: Boolean },
+      _firstStateBundle: { type: Boolean },
+      _managerEle: { type: Object },
+      _adminEle: { type: Object },
+      _renderEle: { type: Object },
+      _playerEle: { type: Object },
     }
   }
 
-  ready() {
-    super.ready();
+  constructor() {
+    super();
 
-    this.addEventListener('propose-move', e => this.handleProposeMove(e));
+    this.requestedPlayer = 0;
+    this.promptedToJoin = false;
+    this._firstStateBundle = true;
+    this.viewingAsPlayer = 0;
+
+    this.addEventListener('propose-move', e => this._handleProposeMove(e));
     this.addEventListener('refresh-info', e => this._handleRefreshData(e));
     this.addEventListener('install-state-bundle', e => this._handleStateBundle(e));
     this.addEventListener('install-game-static-info', e => this._handleGameStaticInfo(e));
@@ -128,37 +105,52 @@ class BoardgameGameView extends PolymerElement {
   }
 
   _handleRefreshData(e) {
-    this.$.manager.fetchInfo();
+    this._managerEle.fetchInfo();
   }
 
-  handleProposeMove(e) {
-    let adminEle = this.shadowRoot.querySelector("#admin");
+  _handleRequestedPlayerChanged(e) {
+    this.requestedPlayer = e.detail.value;
+  }
 
-    if (!adminEle) {
-      console.warn("propose-move fired, but no moves element to forward to.");
-      return;
+  _handleAutoCurrentPlayerChanged(e) {
+    this.autoCurrentPlayer = e.detail.value;
+  }
+
+  _handleSocketActiveChanged(e) {
+    this.socketActive = e.detail.value;
+  }
+
+  _handleRendererChanged(e) {
+    this.activeRenderer = e.detail.value;
+  }
+
+  _handleProposeMove(e) {
+    this._adminEle.proposeMove(e.detail.name, e.detail.arguments);
+  }
+
+  firstUpdated() {
+    this._managerEle = this.shadowRoot.querySelector("#manager");
+    this._adminEle = this.shadowRoot.querySelector("#admin");
+    this._renderEle = this.shadowRoot.querySelector("#render");
+    this._playerEle = this.shadowRoot.querySelector("#player");
+  }
+
+  updated(changedProps) {
+    if (changedProps.has('selected') && !this.selected) {
+      this._resetState();
     }
-
-    adminEle.proposeMove(e.detail.name, e.detail.arguments);
-  }
-
-  _selectedChanged(newValue) {
-    if (!newValue) {
+    if (changedProps.has('gameRoute')) {
+      //reset this so the next time we get data set and notice that we COULD
+      //login we prompt for it.
+      this.promptedToJoin = false;
       this._resetState();
     }
   }
 
-  _gameRouteChanged() {
-    //reset this so the next time we get data set and notice that we COULD
-    //login we prompt for it.
-    this.promptedToJoin = false;
-    this._resetState();
-  }
-
-  doTick() {
+  _doTick() {
     this._tick();
     if (this.pathsToTick.length > 0) {
-      window.requestAnimationFrame(this.doTick.bind(this));
+      window.requestAnimationFrame(this._doTick.bind(this));
     }
   }
 
@@ -168,26 +160,29 @@ class BoardgameGameView extends PolymerElement {
 
     let newPaths = [];
 
-    let pathToExpanded = ["currentState"]
-
     for (let i = 0; i < this.pathsToTick.length; i++) {
       let currentPath = this.pathsToTick[i];
 
-      let pathToFetch = pathToExpanded.concat(currentPath);
-
-      let timer = this.get(pathToFetch);
+      let timer = getProperty(this.currentState, currentPath);
 
       let now = Date.now();
       let difference = now - this.originalWallClockStartTime;
 
       let result = Math.max(0, timer.originalTimeLeft - difference);
 
-      this.set(pathToExpanded.concat(currentPath).concat(["TimeLeft"]), result);
+      let newState = deepCopy(this.currentState);
+
+      if (!setProperty(newState, currentPath.concat(["TimeLeft"]), result)) {
+        console.warn("Failed to set property: ", newState, currentPath.concat("TimeLeft"), result);
+      }
+
+      //this should requestUpdate automatically since it's a copy
+      this.currentState = newState;
 
       //If we still have time to tick on this, then make sure it's still
       //in the list of things to tick.
       if (timer.TimeLeft > 0) {
-        newPaths.push(currentPath)
+        newPaths.push(currentPath);
       }
     }
 
@@ -199,15 +194,18 @@ class BoardgameGameView extends PolymerElement {
   }
 
   _handleGameStaticInfo(e) {
-    this.setProperties(e.detail);
+    //this is where things like chest, gameOpen, gameVisible, hasEmptySlots, isOwner, and playersInfo is set.
+    for (let [key, val] of Object.entries(e.detail)) {
+      this[key] = val;
+    }
   }
 
   _handleAllAnimationsDone(e) {
-    this.$.manager.readyForNextState();
+    this._managerEle.readyForNextState();
   }
 
   _handleSetAnimationLength(e) {
-    this.$.render.defaultAnimationLength = e.detail;
+    this._renderEle.defaultAnimationLength = e.detail;
   }
 
   _firstStateBundleInstalled() {
@@ -216,7 +214,7 @@ class BoardgameGameView extends PolymerElement {
       //Take note that we already prompted them, and don't prompt again unless the game changes.
       this.promptedToJoin = true;
       //Prompt the user to join!
-      this.$.player.showDialog();
+      this._playerEle.showDialog();
     }
   }
 
@@ -241,23 +239,22 @@ class BoardgameGameView extends PolymerElement {
 
   _installStateBundle(bundle) {
 
-    this.setProperties({
-      game: bundle.game,
-      currentState: bundle.game.CurrentState,
-      moveForms: bundle.moveForms,
-      viewingAsPlayer: bundle.viewingAsPlayer,
-      originalWallClockStartTime: bundle.originalWallClockStartTime,
-      gameState: bundle.gameState,
-      pathsToTick: bundle.pathsToTick
-    })
+    //We only rerender once despite setting multiple properties at once
+    this.game = bundle.game;
+    this.currentState = bundle.game.CurrentState;
+    this.moveForms = bundle.moveForms;
+    this.viewingAsPlayer = bundle.viewingAsPlayer;
+    this.originalWallClockStartTime = bundle.originalWallClockStartTime;
+    this.gameState = bundle.gameState;
+    this.pathsToTick = bundle.pathsToTick;
 
     if (this._firstStateBundle) {
       this._firstStateBundleInstalled();
     }
     this._firstStateBundle = false;
 
-    window.requestAnimationFrame(() => this.doTick());
+    window.requestAnimationFrame(() => this._doTick());
   }
 }
 
-customElements.define(BoardgameGameView.is, BoardgameGameView);
+customElements.define('boardgame-game-view', BoardgameGameView);
